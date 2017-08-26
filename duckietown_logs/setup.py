589607698337b711374e40b4371b9b1023d3caf3
Andrea Censi <acensi@idsc.mavt.ethz.ch>
2017-08-26 18:05:34 +0200

    commit

--- duckietown_logs/setup.py
+++ duckietown_logs/setup.py
@@ -6,15 +6,12 @@ setup_args = generate_distutils_setup(
     packages=['duckietown_logs'],
     package_dir={'': 'include'},
     install_requires = [
-     'SystemCmd==1.2', 
-     'ros_node_utils==1.0', 
-     'ConfTools==1.8',
-     'QuickApp==1.2.2',
-     'Procgraph==1.10',
+     'SystemCmd>=1.2', 
+     'ros_node_utils>=1.0', 
+     'ConfTools>=1.8',
+     'QuickApp>=1.2.2',
+     'Procgraph>=1.10',
     ]
 )
-
-#print(setup_args)
-
-#raise ValueError('are you even executing this?')
+ 
 setup(**setup_args)
