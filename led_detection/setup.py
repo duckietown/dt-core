954b4a104f7aaf8c8e69227c8d5cc3c738fa3f4a
Andrea Censi <acensi@idsc.mavt.ethz.ch>
2017-08-26 14:47:50 +0200

    Clean up

--- led_detection/setup.py
+++ led_detection/setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
     packages=[
-	'led_detection',
+        'led_detection',
     ],
     install_requires=[],
     package_dir={'': 'include'},
