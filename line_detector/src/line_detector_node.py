6c533844fcbb18a55acbd8ba4d56339b05fd5978
lpaull <lpaull@mit.edu>
2017-10-11 17:15:59 -0400

    brining line detector up to date with new SegmentList definition

--- line_detector/src/line_detector_node.py
+++ line_detector/src/line_detector_node.py
@@ -245,10 +245,10 @@ class LineDetectorNode(object):
         for x1,y1,x2,y2,norm_x,norm_y in np.hstack((lines,normals)):
             segment = Segment()
             segment.color = color
-            segment.pixels_normalized[0].x = x1
-            segment.pixels_normalized[0].y = y1
-            segment.pixels_normalized[1].x = x2
-            segment.pixels_normalized[1].y = y2
+            segment.pixels_normalized[0].u = x1
+            segment.pixels_normalized[0].v = y1
+            segment.pixels_normalized[1].u = x2
+            segment.pixels_normalized[1].v = y2
             segment.normal.x = norm_x
             segment.normal.y = norm_y
              
