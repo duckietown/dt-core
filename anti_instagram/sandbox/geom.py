615dba863b732b1232d029b3acabc886d1d5aca3
David Yunis <dyunis@uchicago.edu>
2017-12-04 18:15:54 -0600

    geom includes lane_surface

--- anti_instagram/sandbox/geom.py
+++ anti_instagram/sandbox/geom.py
@@ -3,59 +3,110 @@ import cv2
 import numpy as np
 import sys
 
-def processGeom(img):
-    #img = cv2.resize(img, (img.shape[0]/3, img.shape[0]/3),interpolation=cv2.INTER_NEAREST)
-    top_cutoff = img.shape[0]/3
-    img = img[top_cutoff:,:,:]
+#the main function is processGeom, it works in 3 steps:
+#1) identify the surface of the road, we don't want to look at anything extraneous
+#2) filter the image based on some color thresholds to identify regions of different colors (colorFilter)
+#3) find the contours within that filter and fill them in to form a mask for regions of interest 
+
+#threshold values for the colorFilter, taken from line_detector and then tweaked
+hsv_white1 = np.array([0,0,150])
+hsv_white2 = np.array([180,90,255])
+hsv_yellow1 = np.array([20,120,150])
+hsv_yellow2 = np.array([45,255,255])
+hsv_red1 = np.array([0,120,100])
+hsv_red2 = np.array([12,255,255])
+hsv_red3 = np.array([165,120,100])
+hsv_red4 = np.array([180,255,255])
+
+def processGeom(img, viz=False):
+    #first narrow scope to surface of lane to avoid extraneous info
+    surf = identifyLaneSurface(img, use_hsv=False, grad_thresh=30)
+    orig = img
+    img = np.expand_dims(surf,-1)*img
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     colors = ['white','yellow','red']
-    disp = np.zeros((hsv.shape[0],hsv.shape[1],3),dtype=np.uint8)
     masks = {}
-    cmasks = {}
-    disp2 = np.zeros_like(img)
     for color in colors:
-    	bw = colorFilter(hsv,color)
-    	#masks[color] = detectColor(bw,color)
-	contours = detectColor2(bw)
-	#masks[color] = cv2.cvtColor(masks[color], cv2.COLOR_GRAY2BGR)
-	if color=='white':
-	    #masks[color] *= np.array([255,255,255], dtype=np.uint8)
-	    col = (255,255,255)
-	if color=='yellow':
-	    #masks[color] *= np.array([0,255,255], dtype=np.uint8)
-	    col = (0,255,255)
-	if color=='red':
-	    #masks[color] *= np.array([0,0,255], dtype=np.uint8)
-	    col = (0,0,255)
-	#disp += masks[color]
-	cmasks[color] = np.zeros(img.shape, dtype=np.uint8)
-	cv2.drawContours(cmasks[color],contours,-1,255,-1)
-	cmasks[color] = np.where(cmasks == 255)
-	cv2.drawContours(disp2,contours,-1,col,-1)
+	#filter colors to get binary mask
+    	bw = colorFilter(hsv,color,thresh=None)
 
-    return disp, disp2
+	#find contours based on binary mask, filter out small noise
+	_, contours, _ = cv2.findContours(bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    	contours = [contour for contour in contours if contour.shape[0] >= 30]
 
-#taken straight out of line_detector1.py
-def colorFilter(img, color):
+	#create the masks, store only when nonempty
+	mask = np.zeros((img.shape[0],img.shape[1]), dtype=np.uint8)
+	cv2.drawContours(mask,contours,-1,1,-1)
+	if len(np.unique(mask))>1:
+	    masks[color] = mask
+
+    if viz:
+	cols = {'white':(255,255,255), 'yellow':(0,255,255), 'red':(0,0,255)}
+	cv2.imshow('img',orig)
+	cv2.waitKey(0)
+	cv2.imshow('img',img)
+	cv2.waitKey(0)
+	tot = np.zeros_like(img)
+	for col in masks:
+	    mimg = np.expand_dims(masks[col],-1)*img
+	    tot += mimg
+	    cv2.imshow('img',mimg)
+	    cv2.waitKey(0)
+	cv2.imshow('img',tot)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+    return masks
+
+#this has problems with specular reflections
+def identifyLaneSurface(img, use_hsv=False, visualize=False, grad_thresh=30):
+    h, w = img.shape[:2]
+    img_for_grad = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) if use_hsv else img
+    dx = cv2.Sobel(img_for_grad, cv2.CV_32F, 1, 0)
+    dy = cv2.Sobel(img_for_grad, cv2.CV_32F, 0, 1)
+    grad = (np.sqrt(np.mean(dx**2 + dy**2, 2)) > grad_thresh).astype(np.uint8)
+    mask = np.zeros((h + 2, w + 2), np.uint8)
+    y = int(h / 2.0 * 1)
+    for x in range(w):
+        if not (grad[y, x] or mask[y + 1, x + 1]):
+            cv2.floodFill(grad, mask, (x, y), 0,
+                          flags=cv2.FLOODFILL_MASK_ONLY)
+    mask[y + 1:, :] = 1
+    mask[0, :] = 0
+    mask = mask[:, 1:-1]
+    mask_ = np.zeros((h + 4, w + 2), np.uint8)
+    cv2.floodFill(mask, mask_, (0, 0), 0, flags=cv2.FLOODFILL_MASK_ONLY)
+    mask = 1 - mask_[2:-2, 1:-1]
+
+    #added to get rid of wall artifacts post filling
+    m = np.zeros_like(mask)
+    m[mask.shape[0]/3:,:] = 1
+    mask *= m
+    return mask
+
+#taken out of line_detector1.py, thresh should be a numpy array of shape (n,3), this is to provide previous estimates
+def colorFilter(img, color,thresh=None):
     # threshold colors in HSV space
     '''TODO: use the previous kmeans estimate +- some range to be the threshold colors'''
-    '''TODO: instead of thresholding for 3 colors (poor performance on pic1, pic3) threshold for the entire white to red to yellow range'''
-    hsv_white1 = np.array([0,0,150])
-    hsv_white2 = np.array([180,60,255])
-    hsv_yellow1 = np.array([25,140,100])
-    hsv_yellow2 = np.array([45,255,255])
-    hsv_red1 = np.array([0,140,100])
-    hsv_red2 = np.array([15,255,255])
-    hsv_red3 = np.array([165,140,100])
-    hsv_red4 = np.array([180,255,255])
-
+    if thresh is not None:
+    	thresh = np.resize(np.uint8(thresh),(thresh.shape[0],1,3))
+    	thresh = cv2.cvtColor(thresh,cv2.COLOR_BGR2HSV)
+	t1,t2 = thresh[0:2,...]
     if color == 'white':
-        bw = cv2.inRange(img, hsv_white1, hsv_white2)
+	if thresh is None:
+	    t1,t2 = hsv_white1, hsv_white2
+        bw = cv2.inRange(img, t1, t2)
     elif color == 'yellow':
-        bw = cv2.inRange(img, hsv_yellow1, hsv_yellow2)
+	if thresh is None:
+	    t1,t2 = hsv_yellow1,hsv_yellow2
+        bw = cv2.inRange(img, t1, t2)
     elif color == 'red':
-        bw1 = cv2.inRange(img, hsv_red1, hsv_red2)
-        bw2 = cv2.inRange(img, hsv_red3, hsv_red4)
+	if thresh is None:
+	    t1,t2,t3,t4 = hsv_red1,hsv_red2,hsv_red3,hsv_red4
+	else:
+	    t3,t4 = thresh[2:4,...]
+        bw1 = cv2.inRange(img, t1, t2)
+        bw2 = cv2.inRange(img, t3, t4)
         bw = cv2.bitwise_or(bw1, bw2)
     else:
         raise Exception('Error: Undefined color strings...')
@@ -63,29 +114,20 @@ def colorFilter(img, color):
     # binary dilation
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3, 3))
     bw = cv2.dilate(bw, kernel)
-    #cv2.imshow('img',bw)
-    #cv2.waitKey(0)
-
     return bw
 
-#the assumption here is that our regions of interest are polygonal
-def detectColor2(bw):
-    im2, contours, hierarchy = cv2.findContours(bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours = [contour for contour in contours if contour.shape[0] >= 30]
-    return contours
-
-#first idea, obsolete
+#geometric primitives, not used
 def detectColor(bw, color):
     n = 20
     if color=='white':
 	#white lines should be perpendicular to horizon
-	w,h = 50,200
+	w,h = 30,80
     elif color=='yellow':
 	#yellow should come in patches, separated by gray space
-	w,h = 80,80
+	w,h = 30,30
     elif color=='red':
 	#red lines are near parallel to the horizon
-	w,h = 200,50
+	w,h = 80,30
     else:
 	raise Exception('Error: Undefined color strings...')
     w,h = 100,100
@@ -97,6 +139,7 @@ def detectColor(bw, color):
     mask = cv2.bitwise_and(bw,mask)
     return mask
 
+#not used
 def contigRegion(bw,kx,ky,n):
     #kernel shape is based on some geometric primitive of lane (should be trapezoid instead of rectangle?)
     kernel = np.ones((ky,kx),np.float32)
@@ -113,11 +156,4 @@ def contigRegion(bw,kx,ky,n):
 if __name__=='__main__':
     fname = sys.argv[1]
     img = cv2.imread(fname)
-    disp, disp2 = processGeom(img)
-    cv2.imshow('img',img)
-    cv2.waitKey(0)
-    cv2.imshow('img',disp)
-    cv2.waitKey(0)
-    cv2.imshow('img',disp2)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    masks = processGeom(img, viz=True)
