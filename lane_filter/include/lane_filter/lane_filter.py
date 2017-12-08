461c980d5a7cde7fd18c5a84a6e13dd94c501313
Simon Muntwiler <simonmu@student.ethz.ch>
2017-12-08 20:36:08 +0100

    Multiple beliefs

--- lane_filter/include/lane_filter/lane_filter.py
+++ lane_filter/include/lane_filter/lane_filter.py
@@ -38,7 +38,7 @@ class LaneFilterHistogram(Configurable, LaneFilterInterface):
         self.num_belief = 3
         self.d,self.phi = np.mgrid[self.d_min:self.d_max:self.delta_d,self.phi_min:self.phi_max:self.delta_phi]
         self.belief = np.empty(self.d.shape)
-        #self.beliefArray = [self.belief] * num_belief
+        self.beliefArray = [np.empty(self.d.shape)] * num_belief
         self.mean_0 = [self.mean_d_0, self.mean_phi_0]
         self.cov_0  = [ [self.sigma_d_0, 0], [0, self.sigma_phi_0] ]
         self.cov_mask = [self.sigma_d_mask, self.sigma_phi_mask]  
