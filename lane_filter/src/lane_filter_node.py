e9375b96c7a5c265ded0ebdd938d8773985930b7
Simon Muntwiler <simonmu@student.ethz.ch>
2017-12-13 11:37:46 +0100

    Left curve decision

--- lane_filter/src/lane_filter_node.py
+++ lane_filter/src/lane_filter_node.py
@@ -64,8 +64,8 @@ class LaneFilterNode(object):
         max_val = self.filter.getMax()
         in_lane = max_val > self.filter.min_max 
 
-        if (d_max[2] - d_max[0] > 0.1):  #&& phi_max[2] - phi_max[0] > -0.04):
-            print "Yuhui I am on a left curve"
+        if (d_max[2] - d_max[0] > 0.1 and phi_max[2] - phi_max[0] > -0.04):
+            print "I see a left curve"
         else:
             print "I don't know where I am"
         
