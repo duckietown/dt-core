86d4295143b72818c04683d4dda530b846dafd2d
Simon Muntwiler <simonmu@student.ethz.ch>
2017-12-15 17:49:47 +0100

    7 belief, output median of d and phi

--- lane_filter/include/lane_filter/lane_filter.py
+++ lane_filter/include/lane_filter/lane_filter.py
@@ -74,15 +74,10 @@ class LaneFilterHistogram(Configurable, LaneFilterInterface):
 
 
     
-    def update(self, segments, range_min, range_max):
-
-        range_delta = (range_max - range_min)/(self.num_belief - 1)
+    def update(self, segments, range_arr):
 
         for i in range(self.num_belief):
-            if i == 0:
-                measurement_likelihood = self.generate_measurement_likelihood(segments, 0, range_min)
-            else:
-                measurement_likelihood = self.generate_measurement_likelihood(segments, range_min + (i-1) * range_delta, range_min + i * range_delta)
+            measurement_likelihood = self.generate_measurement_likelihood(segments, range_arr[i], range_arr[i + 1])
             if measurement_likelihood is not None:
                 self.beliefArray[i] = np.multiply(self.beliefArray[i],measurement_likelihood)
                 if np.sum(self.beliefArray[i]) == 0:
