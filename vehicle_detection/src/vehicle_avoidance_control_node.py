a9e4c1e0fb4b901c2f04d1c1197d2734dc009970
LukSchoenbaechler <lukasch@student.ethz.ch>
2017-12-18 12:42:19 +0100

    added parameterfile

--- vehicle_detection/src/vehicle_avoidance_control_node.py
+++ vehicle_detection/src/vehicle_avoidance_control_node.py
@@ -23,9 +23,64 @@ class VehicleAvoidanceControlNode(object):
 		self.sub_vehicle_pose = rospy.Subscriber("~vehicle_pose", VehiclePose, self.cbPose, queue_size=1)
 		self.sub_car_cmd = rospy.Subscriber("~car_cmd_in", Twist2DStamped, self.cbCarCmd, queue_size=1)
 		
-		self.v_gain = 1
+		self.config	= self.setupParam("~config", "baseline")
+		self.cali_file_name = self.setupParam("~cali_file_name", "default")
+		rospack = rospkg.RosPack()
+		self.cali_file = rospack.get_path('duckietown') + \
+				"/config/" + self.config + \
+				"/vehicle_detection/vehicle_avoidance_control_node/" +  \
+				self.cali_file_name + ".yaml"
+		if not os.path.isfile(self.cali_file):
+			rospy.logwarn("[%s] Can't find calibration file: %s.\n" 
+					% (self.node_name, self.cali_file))
+		self.loadConfig(self.cali_file)
+		self.controllerInitialization()
+		
+# 		self.v_gain = 1
+# 		self.vehicle_pose_msg_temp = VehiclePose()
+# 		#self.vehicle_pose_msg_temp = Pose2DStamped()
+# 		self.vehicle_pose_msg_temp.header.stamp = rospy.Time.now()
+# 		#self.time_temp = rospy.Time.now()
+# 		self.v_rel = 0
+# 		self.v = 0
+# 		self.detection = False
+# 		self.v_error_temp = 0
+# 		self.I = 0
+# 		self.v_follower = 0
+# 		self.rho_temp = 0
+# 		self.omega = 0
+
+	def setupParam(self, param_name, default_value):
+		value = rospy.get_param(param_name, default_value)
+		rospy.set_param(param_name, value)
+		rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
+		return value
+	
+	def loadConfig(self, filename):
+		stream = file(filename, 'r')
+		data = yaml.load(stream)
+		stream.close()
+		self.desired_distance = data['desired_distance']
+		rospy.loginfo('[%s] desired_distance : %s' % (self.node_name, 
+				self.desired_distance))
+		self.minimal_distance = data['minimal_distance']
+		rospy.loginfo('[%s] minimal_distance : %s' % (self.node_name, 
+				self.minimal_distance))
+		self.Kp = data['Kp']
+		rospy.loginfo('[%s] Kp : %s' % (self.node_name, 
+				self.Kp))
+		self.Ki = data['Ki']
+		rospy.loginfo('[%s] Ki : %s' % (self.node_name, 
+				self.Ki))
+		self.Kd = data['Kd']
+		rospy.loginfo('[%s] Kd : %s' % (self.node_name, 
+				self.Kd))
+		self.Kp_delta_v = data['Kp_delta_v']
+		rospy.loginfo('[%s] Kp_delta_v : %s' % (self.node_name, 
+				self.Kp_delta_v))
+		
+	def controllerInitialization(self):
 		self.vehicle_pose_msg_temp = VehiclePose()
-		#self.vehicle_pose_msg_temp = Pose2DStamped()
 		self.vehicle_pose_msg_temp.header.stamp = rospy.Time.now()
 		#self.time_temp = rospy.Time.now()
 		self.v_rel = 0
@@ -37,21 +92,8 @@ class VehicleAvoidanceControlNode(object):
 		self.rho_temp = 0
 		self.omega = 0
 
-	def setupParam(self, param_name, default_value):
-		value = rospy.get_param(param_name, default_value)
-		rospy.set_param(param_name, value)
-		rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
-		return value
-
 	def callback(self, data):
-#                 vehicle_too_close = BoolStamped()
-#                 vehicle_too_close.header.stamp = data.header.stamp
-# 		if not data.data:
-# 			vehicle_too_close.data = False
-# 		else:
-# 			vehicle_too_close.data = True
-# 		self.publishCmd(data.header.stamp)
-# 		self.vehicle_detected_pub.publish(vehicle_too_close)
+
 		vehicle_detected_msg_out = BoolStamped()
 		vehicle_detected_msg_out.header.stamp = data.header.stamp
 		vehicle_detected_msg_out.data = data.data
@@ -65,7 +107,7 @@ class VehicleAvoidanceControlNode(object):
 			
 		
 	def cbPose(self, vehicle_pose_msg):
-		d_desired = 0.3
+		desired_distance = 0.3
 		#distance_error_tolerance = 0.04
 		d_min = 0.2
 		Kp = 0.7
@@ -74,9 +116,7 @@ class VehicleAvoidanceControlNode(object):
 		Kd = 0.00
 		
 		time = rospy.Time.now()
-		
-		#rho = sqrt(vehicle_pose_msg.x**2 + vehicle_pose_msg.y**2)
-		
+				
 		Ts = (vehicle_pose_msg.header.stamp - self.vehicle_pose_msg_temp.header.stamp).to_sec()
 		#Ts2 = (time - self.time_temp).to_sec()
 # 		print("-----")
@@ -84,23 +124,16 @@ class VehicleAvoidanceControlNode(object):
 # 		print(Ts2)
 		self.vehicle_pose_msg_temp.header.stamp = vehicle_pose_msg.header.stamp
 		#print(Ts)
-		if Ts > 3:
+		if Ts > 2:
 			self.v_rel = 0
 			self.v = 0
 			self.vehicle_pose_msg_temp = vehicle_pose_msg
-			#self.rho_temp = rho
 			self.v_error_temp = 0
 			self.I = 0
 		else:
 			self.v_rel = (vehicle_pose_msg.rho.data - self.vehicle_pose_msg_temp.rho.data)/Ts
 			v_leader = self.v_follower + self.v_rel
-			#if v_leader < 0.2:
-			#	v_leader = 0
-			print(v_leader)
-			#if (vehicle_pose_msg.rho.data - d_desired) > distance_error_tolerance:
-			delta_v = (vehicle_pose_msg.rho.data - d_desired)/Ts * Kp_delta_v
-			#else:
-			#	delta_v = 0
+			delta_v = (vehicle_pose_msg.rho.data - self.desired_distance)/Ts * self.Kp_delta_v
 			v_des = v_leader + delta_v
 			
 # 			print("v leader")
@@ -118,14 +151,13 @@ class VehicleAvoidanceControlNode(object):
 			
 			v_error = v_des - self.v_follower
 
-			self.P = Kp*v_error
-			self.I = self.I + Ki * (v_error + self.v_error_temp)/2.0*Ts 
-			self.D = Kd * (v_error + self.v_error_temp)/Ts 
+			self.P = self.Kp*v_error
+			self.I = self.I + self.Ki * (v_error + self.v_error_temp)/2.0*Ts 
+			self.D = self.Kd * (v_error + self.v_error_temp)/Ts 
 			self.v = self.P + self.I + self.D
 			
-			if self.v < 0 or vehicle_pose_msg.rho.data < d_min:
+			if self.v < 0 or vehicle_pose_msg.rho.data < self.minimal_distance:
 				self.v = 0
-				#self.omega = 0
 			
 			#self.rho_temp = rho
 			self.v_error_temp = v_error
@@ -146,14 +178,12 @@ class VehicleAvoidanceControlNode(object):
 	def cbCarCmd(self, car_cmd_msg):
 		car_cmd_msg_current = Twist2DStamped()
 		car_cmd_msg_current = car_cmd_msg
-		#car_cmd_msg_current.v = self.v_gain * car_cmd_msg_current.v
 		if self.detection:
 			car_cmd_msg_current.v = self.v
 			if self.v == 0:
 				car_cmd_msg_current.omega = 0
 			#print(self.v)
 		self.v_follower = car_cmd_msg_current.v	
-		#car_cmd_msg_current.omega = 0.0
 		self.car_cmd_pub.publish(car_cmd_msg_current)
 		#print(self.v_gain)
 		
