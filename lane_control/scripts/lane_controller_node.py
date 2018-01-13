1d7860d01941cf4888197b42c4c7f851c6c77d7d
Fabrice Oehler <foehler@ethz.ch>
2018-01-13 16:02:42 +0100

    removed further prints

--- lane_control/scripts/lane_controller_node.py
+++ lane_control/scripts/lane_controller_node.py
@@ -186,8 +186,8 @@ class lane_controller(object):
     def setFlag(self, msg_flag, flag_name):
         self.flag_dict[flag_name] = msg_flag.data
         if flag_name == "obstacle_detected":
-            print "flag obstacle_detected changed"
-            print "flag_dict[\"obstacle_detected\"]: ", self.flag_dict["obstacle_detected"]
+            # print "flag obstacle_detected changed"
+            # print "flag_dict[\"obstacle_detected\"]: ", self.flag_dict["obstacle_detected"]
         
     
     def PoseHandling(self, input_pose_msg, pose_source):
@@ -195,9 +195,9 @@ class lane_controller(object):
             return
 
         if pose_source == "obstacle_avoidance":
-            print "obstacle_avoidance pose_msg d_ref: ", input_pose_msg.d_ref
-            print "obstacle_avoidance pose_msg v_ref: ", input_pose_msg.v_ref
-            print "flag_dict[\"obstacle_detected\"]: ", self.flag_dict["obstacle_detected"]
+            # print "obstacle_avoidance pose_msg d_ref: ", input_pose_msg.d_ref
+            # print "obstacle_avoidance pose_msg v_ref: ", input_pose_msg.v_ref
+            # print "flag_dict[\"obstacle_detected\"]: ", self.flag_dict["obstacle_detected"]
 
         self.prev_pose_msg = self.pose_msg
         self.pose_msg_dict[pose_source] = input_pose_msg
