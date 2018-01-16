7bd206370b30490730e9c3f162507bcd8c915bf0
Niklas Funk <nfunk@ethz.ch>
2018-01-16 22:15:29 +0800

    fast fix

--- anti_instagram/src/image_transformer_node.py
+++ anti_instagram/src/image_transformer_node.py
@@ -30,6 +30,7 @@ class ImageTransformerNode():
         self.r = rospy.Rate(4) # Rate in Hz
         robot_name = rospy.get_param("~veh", "") #to read the name always reliably
         cont_mode = rospy.get_param("~cont_mode", "") #tells which topic to listen to
+        cont_mode = False
 
         # Initialize publishers and subscribers
         self.pub_image = rospy.Publisher(
