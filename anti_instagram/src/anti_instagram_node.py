a502db231226ceb040708bdabba875ea6ff0458f
Simon Bing/Megacity Team <bings@student.ethz.ch>
2018-05-16 19:13:20 +0200

    anti-insta working at 0.2 Hz

--- anti_instagram/src/anti_instagram_node.py
+++ anti_instagram/src/anti_instagram_node.py
@@ -7,12 +7,6 @@ import duckietown_utils as dtu
 from cv_bridge import CvBridge
 from line_detector.timekeeper import TimeKeeper
 from anti_instagram.anti_instagram_imp import AntiInstagram
-#from anti_instagram.AntiInstagram import *
-from anti_instagram.kmeans_rebuild import *
-from anti_instagram.calcLstsqTransform import *
-from anti_instagram.scale_and_shift import *
-from duckietown_utils.jpg import bgr_from_jpg
-
 
 class AntiInstagramNode(object):
 
@@ -32,10 +26,8 @@ class AntiInstagramNode(object):
         #self.pub_anti_instagram = rospy.Publisher("~click",BoolStamped, queue_size=1)
         #self.sub_switch = rospy.Subscriber("~switch",BoolStamped, self.cbSwitch, queue_size=1)
         #self.sub_image = rospy.Subscriber("~uncorrected_image",Image,self.cbNewImage,queue_size=1)
-        self.sub_image = rospy.Subscriber(
-            "~uncorrected_image", CompressedImage, self.cbNewImage, queue_size=1)
-        self.sub_click = rospy.Subscriber(
-            "~click", BoolStamped, self.cbClick, queue_size=1)
+        self.sub_image = rospy.Subscriber("~uncorrected_image", CompressedImage, self.cbNewImage,queue_size=1)
+        self.sub_click = rospy.Subscriber("~click", BoolStamped, self.cbClick, queue_size=1)
 
         # Verbose option
         self.verbose = rospy.get_param('line_detector_node/verbose',True)
@@ -49,24 +41,15 @@ class AntiInstagramNode(object):
 
         self.ai = AntiInstagram()
         self.corrected_image = Image()
-         self.bridge = CvBridge()
-	    # create instance of kMeans
-     	self.fancyGeom = rospy.get_param("~fancyGeom", "")
-        self.n_centers = rospy.get_param("~n_centers", "")
-	    self.blur = rospy.get_param("~blur","")
-	    self.resize = rospy.get_param("~resize","")
-	    self.blur_kernel = rospy.get_param("~blur_kernel")
-        self.KM = kMeansClass(self.n_centers, self.blur, self.resize, self.blur_kernel)
-
-
-	    self.scale = np.array([1, 1, 1])
-	    self.shift = np.array([0, 0, 0])
-	    self.ai_health = 0.1
-
+        self.bridge = CvBridge()
+        self.ai_frequency=0.2
+        self.ai_frequency_inverse=1/self.ai_frequency
+        self.image_msg = None
+        self.click_on = False
         #runs cb Transform every n seconds
         rospy.Timer(rospy.Duration(self.ai_frequency_inverse), self.contTransform)
 
-    def cbNewImage(self, image_msg):
+    def cbNewImage(self,image_msg):
         # memorize image
         self.image_msg = image_msg
 
@@ -76,6 +59,7 @@ class AntiInstagramNode(object):
 
             corrected_image_cv2 = self.ai.applyTransform(cv_image)
             tk.completed('applyTransform')
+
             corrected_image_cv2 = np.clip(corrected_image_cv2, 0, 255).astype(np.uint8)
             self.corrected_image = self.bridge.cv2_to_imgmsg(corrected_image_cv2, "bgr8")
 
@@ -95,11 +79,12 @@ class AntiInstagramNode(object):
             if self.click_on:
                 self.processImage(self.image_msg)
             else:
-                self.transform.s = [0, 0, 0, 1, 1, 1]
+                self.transform.s = [0,0,0,1,1,1]
                 self.pub_transform.publish(self.transform)
                 rospy.loginfo('ai: Color transform is turned OFF!')
 
-    def processImage(self, msg):
+
+    def processImage(self,msg):
         '''
         Inputs:
             msg - CompressedImage - uncorrected image from raspberry pi camera
@@ -112,7 +97,6 @@ class AntiInstagramNode(object):
         rospy.loginfo('ai: Computing color transform...')
         tk = TimeKeeper(msg)
 
-        #cv_image = self.bridge.imgmsg_to_cv2(msg,"bgr8")
         try:
             cv_image = dtu.bgr_from_jpg(msg.data)
         except ValueError as e:
@@ -125,51 +109,16 @@ class AntiInstagramNode(object):
 
         tk.completed('calculateTransform')
 
-        # apply KMeans
-    	self.KM.applyKM(cv_image, fancyGeom=self.fancyGeom)
-
-    	# get the indices of the matched centers
-    	idxBlack, idxRed, idxYellow, idxWhite  = self.KM.determineColor(True, self.KM.trained_centers)
-
-    	# get centers with red
-    	trained_centers = np.array([self.KM.trained_centers[idxBlack], self.KM.trained_centers[idxRed],
-                                self.KM.trained_centers[idxYellow], self.KM.trained_centers[idxWhite]])
-
-    	# get centers w/o red
-    	trained_centers_woRed = np.array([self.KM.trained_centers[idxBlack], self.KM.trained_centers[idxYellow],
-                                self.KM.trained_centers[idxWhite]])
-
-    	# calculate transform with 4 centers
-    	T4 = calcTransform(4, trained_centers)
-    	T4.calcTransform()
-
-    	# calculate transform with 3 centers
-    	T3 = calcTransform(3, trained_centers_woRed)
-    	T3.calcTransform()
-
-    	# compare residuals
-	    #in practice, this is NOT a fair way to compare the residuals, 4 will almost always win out,
-	    #causing a serious red shift in any image that has only 3 colors
-    	if T4.returnResidualNorm() >= T3.returnResidualNorm():
-            self.shift = T4.shift
-            self.scale = T4.scale
-    	else:
-      	    self.shift = T3.shift
-            self.scale = T3.scale
-
-	    self.shift = T3.shift
-        self.scale = T3.scale
-        tk.completed('calculateTransform')
 
         # if health is much below the threshold value, do not update the color correction and log it.
-        if self.ai_health <= 0.001:
+        if self.ai.health <= 0.001:
             # health is not good
 
             rospy.loginfo("Health is not good")
 
         else:
             self.pub_anti_instagram = rospy.Publisher("anti_instagram_node/click",BoolStamped, queue_size=1)
-            self.health.J1 = self.ai_health
+            self.health.J1 = self.ai.health
             self.transform.s[0], self.transform.s[1], self.transform.s[2] = self.ai.shift
             self.transform.s[3], self.transform.s[4], self.transform.s[5] = self.ai.scale
 
@@ -182,7 +131,7 @@ class AntiInstagramNode(object):
         anti_instagram_msg.data = True
         rospy.loginfo('anti_instagram message')
         self.cbClick(anti_instagram_msg)
-        self.pub_anti_instagram.publish(anti_instagram_msg)
+        #self.pub_anti_instagram.publish(anti_instagram_msg)
         rospy.sleep(2.)
         anti_instagram_msg.data = False
 
@@ -195,6 +144,6 @@ if __name__ == '__main__':
     node = AntiInstagramNode()
 
     # Setup proper shutdown behavior
-    # rospy.on_shutdown(node.on_shutdown)
+    #rospy.on_shutdown(node.on_shutdown)
     # Keep it spinning to keep the node alive
 rospy.spin()
