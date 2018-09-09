2b1de61a4ed5e6ae007baed4355ba83d6ed5f5cb
Andrea Censi <acensi@ethz.ch>
2018-09-09 13:56:18 +0200

    fix

--- lane_filter/include/lane_filter/lane_filter_classic.py
+++ lane_filter/include/lane_filter/lane_filter_classic.py
@@ -70,6 +70,11 @@ class LaneFilterClassic(dtu.Configurable, LaneFilterInterface):
 
         self.initialize()
 
+
+    def getStatus(self):
+        """ Returns one of the statuses above """
+        return LaneFilterInterface.GOOD
+
     def initialize(self):
         pos = np.empty(self.d.shape + (2,))
         pos[:, :, 0] = self.d
