cfe0ecd2a06e2980f4deb80b0f4005c2fb07386e
lpaull <lpaull@mit.edu>
2018-10-02 23:02:18 -0400

    replacing the publishing of the belief image that got removed somewhere along the way

--- lane_filter/src/lane_filter_node.py
+++ lane_filter/src/lane_filter_node.py
@@ -133,11 +133,18 @@ class LaneFilterNode(object):
         if self.curvature_res > 0:
             lanePose.curvature = self.filter.getCurvature(d_max[1:], phi_max[1:])
 
-        #TODO (1): this method does not exist, where is it gone? (Julien)
         # publish the belief image
-        #belief_img = self.getDistributionImage(self.filter.belief, segment_list_msg.header.stamp)
+        belief_img = self.getDistributionImage(self.filter.belief, segment_list_msg.header.stamp)
         self.pub_lane_pose.publish(lanePose)
-
+        self.pub_belief_img.publish(belief_img)
+        
+        
+        def getDistributionImage(self, mat, stamp):
+            bridge = CvBridge()
+            img = bridge.cv2_to_imgmsg((255 * mat).astype('uint8'), "mono8")
+            img.header.stamp = stamp
+            return img
+        
         # Latency of Estimation including curvature estimation
         estimation_latency_stamp = rospy.Time.now() - timestamp_now
         estimation_latency = estimation_latency_stamp.secs + estimation_latency_stamp.nsecs/1e9
