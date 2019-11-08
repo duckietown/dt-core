1398f73f669e0aae31617ad545b8240901487d3a
Aleksandar Petrov <7101799+AleksandarPetrov@users.noreply.github.com>
2019-11-08 16:00:50 +0100

    Update calibrate_extrinsics.py

--- packages/complete_image_pipeline/include/complete_image_pipeline/calibrate_extrinsics.py
+++ packages/complete_image_pipeline/include/complete_image_pipeline/calibrate_extrinsics.py
@@ -55,7 +55,7 @@ class CalibrateExtrinsics(D8App):
                 img_msg = rospy.wait_for_message(topic_name, CompressedImage, timeout=10)
                 print('Image captured!')
             except rospy.ROSException as e:
-                print('Didn\'t get any message!: %s' % (e,))
+                print('\n\n\nDidn\'t get any message!: %s\n MAKE SURE YOU USE DT SHELL COMMANDS OF VERSION 4.1.9 OR HIGHER!\n\n\n' % (e,))
 
             bgr = dtu.bgr_from_rgb(dtu.rgb_from_ros(img_msg))
             self.info('Picture taken: %s ' % str(bgr.shape))
