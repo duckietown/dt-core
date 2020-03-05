8b134f775f947864bf931eaa4f154237a180bf1b
Gianmarco Bernasconi <gibernas@ethz.ch>
2020-03-05 13:41:34 +0100

    Cleanup of old files, refactoring of node (DTROS and some rewriting), documentation

---
+++ packages/lane_control/include/controller.py
@@ -0,0 +1,134 @@
+import numpy as np
+
+
+class LaneController:
+    def __init__(self, parameters):
+        self.parameters = parameters
+        self.d_I = 0
+        self.phi_I = 0
+        self.prev_d_err = 0
+        self.prev_phi_err = 0
+
+    def update_parameters(self, parameters):
+        """Updates parameters of LaneController object.
+
+            Args:
+                parameters (:obj:`dict`): dictionary containing the new parameters for LaneController object.
+        """
+        self.parameters = parameters
+
+    def compute_control_action(self, d_err, phi_err, dt, wheels_cmd_exec, stop_line_distance):
+        """Main function, computes the control action given the current error signals.
+
+        Given an estimate of the error, computes a control action (tuple of linear and angular velocity). This is done
+        via a basic PI(D) controller with anti-reset windup logic.
+
+        Args:
+            d_err (:obj:`float`): error in meters in the lateral direction
+            phi_err (:obj:`float`): error in radians in the heading direction
+            dt (:obj:`float`): time since last command update
+            wheels_cmd_exec (:obj:`bool`): confirmation that the wheel commands have been executed (to avoid
+                                           integration while the robot does not move)
+            stop_line_distance (:obj:`float`):  distance of the stop line, None if not detected.
+        Returns:
+            v (:obj:`float`): requested linear velocity in meters/second
+            omega (:obj:`float`): requested angular velocity in radians/second
+        """
+
+        if dt is not None:
+            self.integrate_errors(d_err, phi_err, dt)
+
+        self.d_I = self.adjust_integral(d_err, self.d_I, self.parameters['integral_bounds']['d'],
+                                        self.parameters['d_resolution'])
+        self.phi_I = self.adjust_integral(phi_err, self.phi_I, self.parameters['integral_bounds']['phi'],
+                                          self.parameters['phi_resolution'])
+
+        self.reset_if_needed(d_err, phi_err, wheels_cmd_exec)
+
+        # Scale the parameters linear such that their real value is at 0.22m/s
+        omega = self.parameters['~k_d'] * (0.22 / self.parameters['~v_bar']) * d_err + \
+            self.parameters['~k_theta'] * (0.22 / self.parameters['~v_bar']) * phi_err
+
+        # apply magic conversion factors
+        omega = omega * self.parameters['~omega_to_rad_per_s']
+
+        self.prev_d_err = d_err
+        self.prev_phi_err = phi_err
+
+        v = self.compute_velocity(stop_line_distance)
+
+        return v, omega
+
+    def compute_velocity(self, stop_line_distance):
+        """Linearly decrease velocity if approaching a stop line.
+
+        If a stop line is detected, the velocity is linearly decreased to achieve a better stopping position,
+        otherwise the nominal velocity is returned.
+
+        Args:
+            stop_line_distance (:obj:`float`): distance of the stop line, None if not detected.
+        """
+        if stop_line_distance is None:
+            return self.parameters['~v_bar']
+        else:
+            # 60cm -> v_bar, 15cm -> v_bar/2
+            d1, d2 = 0.8, 0.25
+            a = self.parameters['~v_bar'] / (2 * (d1 - d2))
+            b = self.parameters['~v_bar'] - a * d1
+            v_new = a * stop_line_distance + b
+            v = np.max([self.parameters['~v_bar'] / 2.0, np.min([self.parameters['~v_bar'], v_new])])
+            return v
+
+    def integrate_errors(self, d_err, phi_err, dt):
+        """Integrates error signals in lateral and heading direction.
+        Args:
+            d_err (:obj:`float`): error in meters in the lateral direction
+            phi_err (:obj:`float`): error in radians in the heading direction
+            dt (:obj:`float`): time delay in seconds
+        """
+        self.d_I += d_err * dt
+        self.phi_I += phi_err * dt
+
+    def reset_if_needed(self, d_err, phi_err, wheels_cmd_exec):
+        """Resets the integral error if needed.
+
+        Resets the integral errors in `d` and `phi` if either the error sign changes, or if the robot is completely
+        stopped (i.e. intersections).
+
+        Args:
+            d_err (:obj:`float`): error in meters in the lateral direction
+            phi_err (:obj:`float`): error in radians in the heading direction
+            wheels_cmd_exec (:obj:`bool`): confirmation that the wheel commands have been executed (to avoid
+                                           integration while the robot does not move)
+        """
+        if np.sign(d_err) != np.sign(self.prev_d_err):
+            self.d_I = 0
+        if np.sign(phi_err) != np.sign(self.prev_phi_err):
+            self.phi_I = 0
+        if wheels_cmd_exec[0] == 0 and wheels_cmd_exec[1] == 0:
+            self.d_I = 0
+            self.phi_I = 0
+
+    @staticmethod
+    def adjust_integral(error, integral, bounds, resolution):
+        """Bounds the integral error to avoid windup.
+
+        Adjusts the integral error to remain in defined bounds, and cancels it if the error is smaller than the
+        resolution of the error estimation.
+
+        Args:
+            error (:obj:`float`): current error value
+            integral (:obj:`float`): current integral value
+            bounds (:obj:`dict`): contains minimum and maximum value for the integral
+            resolution (:obj:`float`): resolution of the error estimate
+
+        Returns:
+            integral (:obj:`float`): adjusted integral value
+        """
+        if integral > bounds['top']:
+            integral = bounds['top']
+        elif integral < bounds['bot']:
+            integral = bounds['bot']
+        elif abs(error) < resolution:
+            integral = 0
+        return integral
---
+++ packages/lane_control/src/lane_controller_node.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+import math
+import numpy as np
+import rospy
+
+from duckietown import DTROS, DTPublisher, DTSubscriber
+from duckietown_msgs.msg import Twist2DStamped, LanePose, WheelsCmdStamped, BoolStamped, FSMState, StopLineReading
+
+from lane_controller.new_controller import LaneController
+
+
+class LaneControllerNode(DTROS):
+
+    def __init__(self, node_name):
+
+        # Initialize the DTROS parent class
+        super(LaneControllerNode, self).__init__(node_name=node_name)
+
+        # Add the node parameters to the parameters dictionary
+        self.parameters['~v_bar'] = None
+        self.parameters['~k_d'] = None
+        self.parameters['~k_theta'] = None
+        self.parameters['~k_Id'] = None
+        self.parameters['~k_Iphi'] = None
+        self.parameters['~theta_thres'] = None
+        self.parameters['~d_thres'] = None
+        self.parameters['~d_offset'] = None
+        self.parameters['~velocity_to_m_per_s'] = None
+        self.parameters['~omega_to_rad_per_s'] = None
+        self.parameters['~integral_bounds'] = None
+        self.parameters['~d_resolution'] = None
+        self.parameters['~phi_resolution'] = None
+        self.parameters['~omega_ff'] = None
+        self.parameters['~verbose'] = None
+        self.updateParameters()
+
+        # Initialize variables
+        self.fsm_state = None
+        self.wheels_cmd_executed = WheelsCmdStamped()
+        self.pose_msg = LanePose()
+        self.pose_initialized = False
+        self.pose_msg_dict = dict()
+        self.last_s = None
+        self.stop_line_distance = None
+        self.stop_line_detected = False
+        self.controller = LaneController(self.parameters)
+        self.current_pose_source = 'lane_filter'
+
+        # Construct publishers
+        self.pub_car_cmd = DTPublisher("~car_cmd", Twist2DStamped, queue_size=1)
+
+        # Construct subscribers
+        self.sub_lane_reading = DTSubscriber("~lane_pose",
+                                             LanePose,
+                                             self.cbAllPoses,
+                                             "lane_filter",
+                                             queue_size=1)
+        self.sub_intersection_navigation_pose = DTSubscriber("~intersection_navigation_pose",
+                                                             LanePose,
+                                                             self.cbAllPoses,
+                                                             "intersection_navigation",
+                                                             queue_size=1)
+        self.sub_wheels_cmd_executed = DTSubscriber("~wheels_cmd_executed",
+                                                    WheelsCmdStamped,
+                                                    self.updateWheelsCmdExecuted,
+                                                    queue_size=1)
+        self.sub_stop_line = rospy.Subscriber("~stop_line_reading",
+                                              StopLineReading,
+                                              self.cbStopLineReading,
+                                              queue_size=1)
+        self.log("Initialized!")
+
+    def cbStopLineReading(self, msg):
+        """Callback storing current distance to the next stopline, if one is detected.
+
+        Args:
+            msg (:obj:`StopLineReading`): Message containing information about the next stop line.
+        """
+        self.stop_line_distance = np.sqrt(msg.stop_line_point.x**2 + msg.stop_line_point.y**2 + msg.stop_line_point.z**2)
+        self.stop_line_detected = msg.stop_line_detected
+
+    def cbMode(self, fsm_state_msg):
+
+        self.fsm_state = fsm_state_msg.state    # String of current FSM state
+
+        if self.fsm_state == 'INTERSECTION_CONTROL':
+            self.current_pose_source = 'intersection_navigation'
+        else:
+            self.current_pose_source = 'lane_filter'
+
+        if self.parameters['~verbose'] == 2:
+            self.log("Pose source: %s" %  self.current_pose_source)
+
+    def cbAllPoses(self, input_pose_msg, pose_source):
+        """Callback receiving pose messages from multiple topics.
+
+        If the source of the message corresponds with the current wanted pose source, it computes a control command.
+
+        Args:
+            input_pose_msg (:obj:`LanePose`): Message containing information about the current lane pose.
+            pose_source (:obj:`String`): Source of the message, specified in the subscriber.
+        """
+
+        if pose_source == self.current_pose_source:
+            self.pose_msg_dict[pose_source] = input_pose_msg
+
+            self.pose_msg = input_pose_msg
+
+            self.getControlAction(self.pose_msg)
+
+    def cbWheelsCmdExecuted(self, msg_wheels_cmd):
+        self.wheels_cmd_executed = msg_wheels_cmd
+
+    def publishCmd(self, car_cmd_msg):
+        self.pub_car_cmd.publish(car_cmd_msg)
+
+    def getControlAction(self, pose_msg):
+        """Callback that receives a pose message and publishes a car command.
+
+        Using a controller object, computes the control action using the current pose estimate.
+
+        Args:
+            pose_msg (:obj:`LanePose`): Message containing information about the current lane pose.
+        """
+
+        # Compute errors
+        d_err = pose_msg.d - self.parameters['~d_offset']
+        phi_err = pose_msg.phi
+
+        # We cap the error if it grows too large
+        if math.fabs(d_err) > self.parameters['~d_thres']:
+            self.log("d_err too large, thresholding it!", 'error')
+            d_err = np.sign(d_err) * self.parameters['~d_thres']
+
+        current_s = rospy.Time.now().to_sec()
+
+        dt = None
+        if self.last_s is not None:
+            dt = (current_s - self.last_s)
+
+        wheels_cmd_exec = [self.wheels_cmd_executed.vel_left, self.wheels_cmd_executed.vel_right]
+        v, omega = self.controller.compute_control_action(d_err, phi_err, dt, wheels_cmd_exec, self.stop_line_distance)
+
+        # For feedforward action (i.e. during intersection navigation)
+        omega += self.parameters['~omega_ff']
+
+        # Initialize car control msg, add header from input message
+        car_control_msg = Twist2DStamped()
+        car_control_msg.header = pose_msg.header
+
+        # Add commands to car message
+        car_control_msg.v = v * self.parameters['~velocity_to_m_per_s']
+        car_control_msg.omega = omega
+
+        self.publishCmd(car_control_msg)
+        self.last_s = current_s
+
+    def updateParameters(self):
+        """Updates parameters in the controller object."""
+        super(LaneControllerNode, self).updateParameters()
+        self.controller.update_parameters(self.parameters)
+
+
+if __name__ == "__main__":
+    # Initialize the node
+    lane_controller_node = LaneControllerNode(node_name='lane_controller_node')
+    # Keep it spinning
+    rospy.spin()
