83f9f00cab7ce6e5bb130bbd237875a668d07355
Aleksandar Petrov <aleksanndar1995@gmail.com>
2020-04-23 16:19:11 +0200

    estimate_homography added

--- packages/ground_projection/include/ground_projection/__init__.py
+++ packages/ground_projection/include/ground_projection/__init__.py
@@ -1,6 +1,1 @@
-#from .configuration import *
-#from .ground_projection_geometry import *
-#from .ground_projection_interface import *
-#from .segment import *
-
 from .geometry import Point, GroundProjectionGeometry
--- packages/ground_projection/include/ground_projection/geometry.py
+++ packages/ground_projection/include/ground_projection/geometry.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 class Point:
 
@@ -13,9 +14,10 @@ class Point:
         y = msg.y
         try:
             z = msg.z
-	except AttributeError:
-	    z=0
-	return Point(x,y,z)
+        except AttributeError:
+            z = 0
+        return Point(x, y, z)
+
 
 class GroundProjectionGeometry:
 
@@ -29,7 +31,7 @@ class GroundProjectionGeometry:
         """ Converts a [0,1] X [0,1] representation to [0, W] X [0, H]. """
         x = self.im_width * vec.x
         y = self.im_height * vec.y
-        return Point(x,y)
+        return Point(x, y)
 
     def pixel2vector(self, pixel):
         """ Converts a [0,W] X [0,H] representation to [0, 1] X [0, 1]. """
@@ -64,3 +66,46 @@ class GroundProjectionGeometry:
         pixel.y = image_point[1]
 
         return pixel
+
+    @staticmethod
+    def estimate_homography(cv_image_rectified, board_w, board_h, square_size, x_offset, y_offset):
+        """Image should be rectified."""
+
+        cv_image_rectified = cv2.cvtColor(cv_image_rectified, cv2.COLOR_BGR2GRAY)
+
+        ret, corners = cv2.findChessboardCorners(cv_image_rectified, (board_w, board_h),
+                                                 cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_NORMALIZE_IMAGE)
+
+        if ret == False:
+            raise RuntimeError("No corners found in image, or the corners couldn't be rearranged. Make sure that the "
+                               "camera is positioned correctly.")
+
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.01)
+        corners_subpix = cv2.cornerSubPix(cv_image_rectified, corners, (11, 11), (-1, -1), criteria)
+
+        src_pts = []
+        for r in range(board_h):
+            for c in range(board_w):
+                src_pts.append(
+                    np.array([r * square_size, c * square_size], dtype='float32') + np.array([x_offset -y_offset]))
+
+        # OpenCV labels corners left-to-right, top-to-bottom
+        # We're having a problem with our pattern since it's not rotation-invariant
+
+        # only reverse order if first point is at bottom right corner
+        if (corners[0])[0][0] < (corners[self.board_['width'] * self.board_['height'] - 1])[0][0] and \
+           (corners[0])[0][0] < (corners[self.board_['width'] * self.board_['height'] - 1])[0][1]:
+            src_pts.reverse()
+
+        # Compute homography from image to ground
+        H, _ = cv2.findHomography(corners_subpix.reshape(len(corners_subpix), 2), np.array(src_pts), cv2.RANSAC)
+
+        if (self.H[1][2] > 0):
+            status = "Homography could be corrupt!"
+        else:
+            status = None
+
+        return GroundProjectionGeometry(im_height=cv_image_rectified.shape[1],
+                                        im_width=cv_image_rectified.shape[0],
+                                        homography=H), \
+               status
