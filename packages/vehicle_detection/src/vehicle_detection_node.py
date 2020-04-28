105c3cad74891bbee303c3d1b26be38add3f70bc
Aleksandar Petrov <aleksanndar1995@gmail.com>
2020-04-28 21:38:32 +0200

    documenting and towards new DTROS

--- packages/vehicle_detection/src/vehicle_detection_node.py
+++ packages/vehicle_detection/src/vehicle_detection_node.py
@@ -3,7 +3,7 @@
 import cv2
 import rospy
 from cv_bridge import CvBridge
-from duckietown import DTROS
+from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
 from sensor_msgs.msg import CompressedImage, Image
 
 from duckietown_msgs.msg import BoolStamped, VehicleCorners
@@ -11,63 +11,95 @@ from geometry_msgs.msg import Point32
 
 
 class VehicleDetectionNode(DTROS):
+    """
+    This node detects if there is another Duckiebot in the image. This is done by recognizing the pattern of circles on
+    the back of every robot.
+
+    Args:
+        node_name (:obj:`str`): a unique, descriptive name for the node that ROS will use
+
+    Configuration:
+        ~process_frequency (:obj:`float`): Frequency at which to process the incoming images
+        ~circlepattern_dims (:obj:`list` of `int`): Number of dots in the pattern, two elements: [number of columns, number of rows]
+        ~blobdetector_min_area (:obj:`int`): Parameter for the blob detector, passed to `SimpleBlobDetector <https://docs.opencv.org/4.3.0/d0/d7a/classcv_1_1SimpleBlobDetector.html>`_
+        ~blobdetector_min_dist_between_blobs (:obj:`str`): Parameter for the blob detector, passed to `SimpleBlobDetector <https://docs.opencv.org/4.3.0/d0/d7a/classcv_1_1SimpleBlobDetector.html>`_
+
+    Subscriber:
+        ~image (:obj:`sensor_msgs.msg.CompressedImage`): Input image
+
+    Publishers:
+        ~centers (:obj:`duckietown_msgs.msg.VehicleCorners`): Detected pattern (if any)
+        ~debug/detection_image/compressed (:obj:`sensor_msgs.msg.CompressedImage`): Debug image that shows the detected pattern
+
+    """
 
     def __init__(self, node_name):
 
         # Initialize the DTROS parent class
-        super(VehicleDetectionNode, self).__init__(node_name=node_name)
+        super(VehicleDetectionNode, self).__init__(
+            node_name=node_name,
+            node_type=NodeType.PERCEPTION
+        )
 
-        # Add the node parameters to the parameters dictionary and load their default values
-        self.parameters['~process_frequency'] = None
-        self.parameters['~circlepattern_dims'] = None
-        self.parameters['~blobdetector_min_area'] = None
-        self.parameters['~blobdetector_min_dist_between_blobs'] = None
-        self.updateParameters()
-
+        # Initialize the parameters
+        self.process_frequency = DTParam('~process_frequency',
+                                         param_type=ParamType.FLOAT)
+        self.circlepattern_dims = DTParam('~circlepattern_dims',
+                                          param_type=ParamType.LIST)
+        self.blobdetector_min_area = DTParam('~blobdetector_min_area',
+                                             param_type=ParamType.FLOAT)
+        self.blobdetector_min_dist_between_blobs = DTParam('~blobdetector_min_dist_between_blobs',
+                                                           param_type=ParamType.FLOAT)
 
         self.bridge = CvBridge()
 
         self.last_stamp = rospy.Time.now()
 
         # Subscriber
-        self.sub_image = self.subscriber("~image", CompressedImage, self.cbImage, queue_size=1)
+        self.sub_image = rospy.Subscriber("~image", CompressedImage, self.cb_image, queue_size=1)
 
         # Publishers
-        # self.pub_detection = self.publisher("~detection", BoolStamped, queue_size=1)
-        self.pub_centers = self.publisher("~centers", VehicleCorners, queue_size=1)
-        self.pub_circlepattern_image = self.publisher("~debug/detection_image/compressed", CompressedImage, queue_size=1)
+        self.pub_centers = rospy.Publisher("~centers", VehicleCorners, queue_size=1)
+        self.pub_circlepattern_image = rospy.Publisher("~debug/detection_image/compressed", CompressedImage, queue_size=1)
 
         self.log("Initialization completed.")
 
     def cbParametersChanged(self):
 
-        self.publish_duration = rospy.Duration.from_sec(1.0/self.parameters['~process_frequency'])
+        self.publish_duration = rospy.Duration.from_sec(1.0/self.process_frequency)
         params = cv2.SimpleBlobDetector_Params()
-        params.minArea = self.parameters['~blobdetector_min_area']
-        params.minDistBetweenBlobs = self.parameters['~blobdetector_min_dist_between_blobs']
+        params.minArea = self.blobdetector_min_area
+        params.minDistBetweenBlobs = self.blobdetector_min_dist_between_blobs
         self.simple_blob_detector = cv2.SimpleBlobDetector_create(params)
 
-    def cbImage(self, image_msg):
+    def cb_image(self, image_msg):
+        """
+        Callback for processing a image which potentially contains a back pattern. Processes the image only if
+        sufficient time has passed since processing the previous image (relative to the chosen processing frequency).
+
+        The pattern detection is performed using OpenCV's `findCirclesGrid <https://docs.opencv.org/2.4/modules/calib3d/doc/camera_calibration_and_3d_reconstruction.html?highlight=solvepnp#findcirclesgrid>`_ function.
+
+        Args:
+            image_msg (:obj:`sensor_msgs.msg.CompressedImage`): Input image
+
+        """
         now = rospy.Time.now()
         if now - self.last_stamp < self.publish_duration:
             return
         else:
             self.last_stamp = now
         
-        # vehicle_detected_msg_out = BoolStamped()
         vehicle_centers_msg_out = VehicleCorners()
         image_cv = self.bridge.compressed_imgmsg_to_cv2(image_msg, "bgr8")
 
         (detection, centers) = cv2.findCirclesGrid(image_cv,
-                                                   patternSize=tuple(self.parameters['~circlepattern_dims']),
+                                                   patternSize=tuple(self.circlepattern_dims),
                                                    flags=cv2.CALIB_CB_SYMMETRIC_GRID,
                                                    blobDetector=self.simple_blob_detector)
 
         # if the pattern is detected, cv2.findCirclesGrid returns a non-zero result, otherwise it returns 0
         # vehicle_detected_msg_out.data = detection > 0
         # self.pub_detection.publish(vehicle_detected_msg_out)
-
-        #print(centers)
 
         vehicle_centers_msg_out.header = image_msg.header
         vehicle_centers_msg_out.detection.data = detection > 0
@@ -83,15 +115,14 @@ class VehicleDetectionNode(DTROS):
                 center.z = 0
                 points_list.append(center)
             vehicle_centers_msg_out.corners = points_list
-            vehicle_centers_msg_out.H = self.parameters['~circlepattern_dims'][1]
-            vehicle_centers_msg_out.W = self.parameters['~circlepattern_dims'][0]
+            vehicle_centers_msg_out.H = self.circlepattern_dims[1]
+            vehicle_centers_msg_out.W = self.circlepattern_dims[0]
 
         self.pub_centers.publish(vehicle_centers_msg_out)
 
-        # TODO: Add check if subscribed
-        if True:
+        if self.pub_circlepattern_image.get_num_connections() > 0:
             cv2.drawChessboardCorners(image_cv,
-                                        tuple(self.parameters['~circlepattern_dims']), centers, detection)
+                                      tuple(self.circlepattern_dims), centers, detection)
             image_msg_out = self.bridge.cv2_to_compressed_imgmsg(image_cv)
             self.pub_circlepattern_image.publish(image_msg_out)
 
--- packages/vehicle_detection/src/vehicle_filter_node.py
+++ packages/vehicle_detection/src/vehicle_filter_node.py
@@ -1,45 +1,59 @@
 #!/usr/bin/env python
 
-
 import cv2
 import rospy
-from cv_bridge import CvBridge
-from sensor_msgs.msg import CompressedImage, Image
-from image_geometry import PinholeCameraModel
-
-from duckietown import DTROS
-from duckietown_msgs.msg import BoolStamped, VehicleCorners
-# from geometry_msgs.msg import Point32
-
-from cv_bridge import CvBridge, CvBridgeError
-from duckietown_msgs.msg import VehicleCorners, VehiclePose, Pose2DStamped, StopLineReading
-from geometry_msgs.msg import Point32
-from mutex import mutex
-from sensor_msgs.msg import CameraInfo
-from visualization_msgs.msg import Marker
-# from math import sqrt, sin, cos
-# from std_msgs.msg import Float32
-import cv2
 import numpy as np
 import os
-import rospkg
-import rospy
-import threading
-import yaml
+from cv_bridge import CvBridge
 
+from image_geometry import PinholeCameraModel
+
+from duckietown.dtros import DTROS, NodeType, TopicType
+from duckietown_msgs.msg import BoolStamped, VehicleCorners, StopLineReading
+from sensor_msgs.msg import CompressedImage, Image, CameraInfo
+from visualization_msgs.msg import Marker
 
 class VehicleFilterNode(DTROS):
+    """
+    The vehicle filter node is responsible for estimating the relative pose to a detected back pattern of a robot and
+    for publishing a stop line message if the other vehicle is too close. We model the vehicle in front as a stop line
+    and leave the velocity control to the lane control node.
+
+    Args:
+        node_name (:obj:`str`): a unique, descriptive name for the node that ROS will use
+
+    Configuration:
+        ~distance_between_centers (:obj:`float`): Distance between the centers of the circles on the back pattern, in meters.
+        ~max_reproj_pixelerror_pose_estimation (:obj:`float`): Maximum tolerable reprojection error. If a reprojection error higher than that is observed. A stop message will be published.
+        ~virtual_stop_line_offset (:obj:`int`): Desired distance from the back pattern of the robot in front, in meters.
+
+    Subscriber:
+        ~centers (:obj:`duckietown_msgs.msg.VehicleCorners`): Detected pattern (if any)
+        ~camera_info (:obj:`sensor_msgs.msg.CameraInfo`): Intrinsic properties of the camera. Needed for rectifying the segments.
+
+    Publishers:
+        ~virtual_stop_line (:obj:`duckietown_msgs.msg.StopLineReading`): Message to the lane controller
+        ~debug/visualization_marker (:obj:`visualization_msgs.msg.Marker`): Debug topic that publishes an rViz marker
+
+    """
 
     def __init__(self, node_name):
 
         # Initialize the DTROS parent class
-        super(VehicleFilterNode, self).__init__(node_name=node_name)
+        super(VehicleFilterNode, self).__init__(
+            node_name=node_name,
+            node_type=NodeType.PERCEPTION
+        )
 
         # Add the node parameters to the parameters dictionary and load their default values
-        self.parameters['~distance_between_centers'] = None
-        self.parameters['~max_reproj_pixelerror_pose_estimation'] = None
-        self.parameters['~virtual_stop_line_offset'] = None
-        self.updateParameters()
+        self.distance_between_centers = DTParam('~distance_between_centers',
+                                                param_type=ParamType.FLOAT)
+
+        self.max_reproj_pixelerror_pose_estimation = DTParam('~max_reproj_pixelerror_pose_estimation',
+                                                             param_type=ParamType.FLOAT)
+
+        self.virtual_stop_line_offset = DTParam('~virtual_stop_line_offset',
+                                                param_type=ParamType.FLOAT)
 
         self.bridge = CvBridge()
 
@@ -49,141 +63,163 @@ class VehicleFilterNode(DTROS):
         self.circlepattern = None
 
         # subscribers
-        self.sub_centers = self.subscriber("~centers", VehicleCorners, self.cb_process_centers, queue_size=1)
-        self.sub_info = self.subscriber("~camera_info", CameraInfo, self.cb_process_camera_info, queue_size=1)
+        self.sub_centers = rospy.Subscriber("~centers", VehicleCorners, self.cb_process_centers, queue_size=1)
+        self.sub_info = rospy.Subscriber("~camera_info", CameraInfo, self.cb_process_camera_info, queue_size=1)
 
         # publishers
-        # self.pub_pose = self.publisher("~pose", VehiclePose, queue_size=1)
-        self.pub_virtual_stop_line = self.publisher("~virtual_stop_line", StopLineReading, queue_size=1)
-        self.pub_visualize = self.publisher("~debug/visualization_marker", Marker, queue_size=1)
+        self.pub_virtual_stop_line = rospy.Publisher("~virtual_stop_line", StopLineReading, queue_size=1)
+        self.pub_visualize = rospy.Publisher("~debug/visualization_marker", Marker, queue_size=1)
 
         self.pcm = PinholeCameraModel()
         self.log("Initialization completed")
 
-    def cb_process_camera_info(self, camera_info_msg):
-        with self.phasetimer.time_phase('cb_process_camera_info callback'):
-            self.pcm.fromCameraInfo(camera_info_msg)
+    def cb_process_camera_info(self, msg):
+        """
+        Callback that stores the intrinsic calibration into a PinholeCameraModel object.
+
+        Args:
+
+            msg (:obj:`sensor_msgs.msg.CameraInfo`): Intrinsic properties of the camera.
+        """
+
+        self.pcm.fromCameraInfo(msg)
 
     def cb_process_centers(self, vehicle_centers_msg):
+        """
+        Callback that processes a back pattern detection. If no detection was made, publishes a dummy stop line message.
+
+        Args:
+            vehicle_centers_msg (:obj:`duckietown_msgs.msg.VehicleCorners`): Detected pattern (if any)
+
+        """
 
         # check if there actually was a detection
         detection = vehicle_centers_msg.detection.data
 
         if detection:
-            with self.phasetimer.time_phase('solve PnP'):
-                self.calc_circle_pattern(vehicle_centers_msg.H, vehicle_centers_msg.W)
-                points = np.zeros((vehicle_centers_msg.H * vehicle_centers_msg.W, 2))
-                for i in range(len(points)):
-                    points[i] = np.array([vehicle_centers_msg.corners[i].x, vehicle_centers_msg.corners[i].y])
+            self.calc_circle_pattern(vehicle_centers_msg.H, vehicle_centers_msg.W)
+            points = np.zeros((vehicle_centers_msg.H * vehicle_centers_msg.W, 2))
+            for i in range(len(points)):
+                points[i] = np.array([vehicle_centers_msg.corners[i].x, vehicle_centers_msg.corners[i].y])
 
-                success, rotation_vector, translation_vector = cv2.solvePnP(objectPoints=self.circlepattern,
-                                                                            imagePoints=points,
-                                                                            cameraMatrix=self.pcm.intrinsicMatrix(),
-                                                                            distCoeffs=self.pcm.distortionCoeffs())
+            success, rotation_vector, translation_vector = cv2.solvePnP(objectPoints=self.circlepattern,
+                                                                        imagePoints=points,
+                                                                        cameraMatrix=self.pcm.intrinsicMatrix(),
+                                                                        distCoeffs=self.pcm.distortionCoeffs())
 
             if success:
-                with self.phasetimer.time_phase('project points and calculate reproj. error'):
-                    points_reproj, _ = cv2.projectPoints(objectPoints=self.circlepattern,
-                                                         rvec=rotation_vector,
-                                                         tvec=translation_vector,
-                                                         cameraMatrix=self.pcm.intrinsicMatrix(),
-                                                         distCoeffs=self.pcm.distortionCoeffs())
+                points_reproj, _ = cv2.projectPoints(objectPoints=self.circlepattern,
+                                                     rvec=rotation_vector,
+                                                     tvec=translation_vector,
+                                                     cameraMatrix=self.pcm.intrinsicMatrix(),
+                                                     distCoeffs=self.pcm.distortionCoeffs())
 
                 mean_reproj_error = np.mean(np.sqrt(np.sum((np.squeeze(points_reproj)-points)**2, axis=1)))
 
-                if mean_reproj_error < self.parameters['~max_reproj_pixelerror_pose_estimation']:
-                    with self.phasetimer.time_phase('calculate pose and publish'):
-                        (R, jac) = cv2.Rodrigues(rotation_vector)
-                        R_inv = np.transpose(R)
-                        translation_vector = -np.dot(R_inv, translation_vector)
-                        distance_to_vehicle = -translation_vector[2]
-                        print("TRANSLATION VECTOR: ", translation_vector)
-                        # make the message and publish
-                        stop_line_msg = StopLineReading()
-                        stop_line_msg.header = vehicle_centers_msg.header
-                        stop_line_msg.stop_line_detected = True
-                        stop_line_msg.at_stop_line = distance_to_vehicle <= self.parameters['~virtual_stop_line_offset']
-                        stop_line_msg.stop_line_point.x = distance_to_vehicle
-                        self.pub_virtual_stop_line.publish(stop_line_msg)
+                if mean_reproj_error < self.max_reproj_pixelerror_pose_estimation:
+                    (R, jac) = cv2.Rodrigues(rotation_vector)
+                    R_inv = np.transpose(R)
+                    translation_vector = -np.dot(R_inv, translation_vector)
+                    distance_to_vehicle = -translation_vector[2]
+
+                    # make the message and publish
+                    self.publish_stop_line_msg(header=vehicle_centers_msg.header,
+                                               detected=True,
+                                               at=distance_to_vehicle <= self.virtual_stop_line_offset,
+                                               x=distance_to_vehicle)
 
                     if self.pub_visualize.get_num_connections() > 0:
-                        with self.phasetimer.time_phase('publish marker'):
-                            marker_msg = Marker()
-                            marker_msg.header = vehicle_centers_msg.header
-                            marker_msg.header.frame_id = 'donald'
-                            marker_msg.ns = 'my_namespace'
-                            marker_msg.id = 0
-                            marker_msg.type = Marker.CUBE
-                            marker_msg.action = Marker.ADD
-                            marker_msg.pose.position.x = -translation_vector[2]
-                            marker_msg.pose.position.y = translation_vector[0]
-                            marker_msg.pose.position.z = translation_vector[1]
-                            marker_msg.pose.orientation.x = 0.0
-                            marker_msg.pose.orientation.y = 0.0
-                            marker_msg.pose.orientation.z = 0.0
-                            marker_msg.pose.orientation.w = 1.0
-                            marker_msg.scale.x = 0.1
-                            marker_msg.scale.y = 0.1
-                            marker_msg.scale.z = 0.1
-                            marker_msg.color.r = 1.0
-                            marker_msg.color.g = 1.0
-                            marker_msg.color.b = 0.0
-                            marker_msg.color.a = 1.0
-                            marker_msg.lifetime = rospy.Duration.from_sec(1)
-                            self.pub_visualize.publish(marker_msg)
+                        marker_msg = Marker()
+                        marker_msg.header = vehicle_centers_msg.header
+                        marker_msg.header.frame_id = 'donald'
+                        marker_msg.ns = 'my_namespace'
+                        marker_msg.id = 0
+                        marker_msg.type = Marker.CUBE
+                        marker_msg.action = Marker.ADD
+                        marker_msg.pose.position.x = -translation_vector[2]
+                        marker_msg.pose.position.y = translation_vector[0]
+                        marker_msg.pose.position.z = translation_vector[1]
+                        marker_msg.pose.orientation.x = 0.0
+                        marker_msg.pose.orientation.y = 0.0
+                        marker_msg.pose.orientation.z = 0.0
+                        marker_msg.pose.orientation.w = 1.0
+                        marker_msg.scale.x = 0.1
+                        marker_msg.scale.y = 0.1
+                        marker_msg.scale.z = 0.1
+                        marker_msg.color.r = 1.0
+                        marker_msg.color.g = 1.0
+                        marker_msg.color.b = 0.0
+                        marker_msg.color.a = 1.0
+                        marker_msg.lifetime = rospy.Duration.from_sec(1)
+                        self.pub_visualize.publish(marker_msg)
 
                 else:
                     self.log("Pose estimation failed, too high reprojection error. "
                              "Reporting detection at 0cm for safety.")
-                    stop_line_msg = StopLineReading()
-                    stop_line_msg.header = vehicle_centers_msg.header
-                    stop_line_msg.stop_line_detected = True
-                    stop_line_msg.at_stop_line = True
-                    stop_line_msg.stop_line_point.x = 0
-                    stop_line_msg.stop_line_point.y = 0
-                    self.pub_virtual_stop_line.publish(stop_line_msg)
+                    self.publish_stop_line_msg(header=vehicle_centers_msg.header,
+                                               detected=True,
+                                               at=True)
 
             else:
                 self.log("Pose estimation failed. "
                          "Reporting detection at 0cm for safety.")
-                stop_line_msg = StopLineReading()
-                stop_line_msg.header = vehicle_centers_msg.header
-                stop_line_msg.stop_line_detected = True
-                stop_line_msg.at_stop_line = True
-                stop_line_msg.stop_line_point.x = 0
-                stop_line_msg.stop_line_point.y = 0
-                self.pub_virtual_stop_line.publish(stop_line_msg)
+                self.publish_stop_line_msg(header=vehicle_centers_msg.header,
+                                           detected=True,
+                                           at=True)
 
         else:
             # publish empty messages
-            stop_line_msg = StopLineReading()
-            stop_line_msg.header = vehicle_centers_msg.header
-            stop_line_msg.stop_line_detected = False
-            stop_line_msg.at_stop_line = False
-            self.pub_virtual_stop_line.publish(stop_line_msg)
+            self.publish_stop_line_msg(header=vehicle_centers_msg.header)
 
-        if self.pub_visualize.get_num_connections() > 0:
-            marker_msg = Marker()
-            marker_msg.header = vehicle_centers_msg.header
-            marker_msg.header.frame_id = 'donald'
-            marker_msg.ns = 'my_namespace'
-            marker_msg.id = 0
-            marker_msg.action = Marker.DELETE
-            self.pub_visualize.publish(marker_msg)
+            if self.pub_visualize.get_num_connections() > 0:
+                marker_msg = Marker()
+                marker_msg.header = vehicle_centers_msg.header
+                marker_msg.header.frame_id = 'donald'
+                marker_msg.ns = 'my_namespace'
+                marker_msg.id = 0
+                marker_msg.action = Marker.DELETE
+                self.pub_visualize.publish(marker_msg)
 
     def calc_circle_pattern(self, height, width):
+        """
+        Calculates the physical locations of each dot in the pattern.
+
+        Args:
+            height (`int`): number of rows in the pattern
+            width (`int`): number of columns in the pattern
+
+        """
         # check if the version generated before is still valid, if not, or first time called, create
 
-        with self.phasetimer.time_phase('calc_circle_pattern callback'):
-            if self.last_calc_circle_pattern is None or self.last_calc_circle_pattern != (height, width):
-                self.circlepattern_dist = self.parameters['~distance_between_centers']
-                self.circlepattern = np.zeros([height * width, 3])
-                for i in range(0, width):
-                    for j in range(0, height):
-                        self.circlepattern[i + j * width, 0] = self.circlepattern_dist * i - \
-                                                               self.circlepattern_dist * (width - 1) / 2
-                        self.circlepattern[i + j * width, 1] = self.circlepattern_dist * j - \
-                                                               self.circlepattern_dist * (height - 1) / 2
+        if self.last_calc_circle_pattern is None or self.last_calc_circle_pattern != (height, width):
+            self.circlepattern_dist = self.distance_between_centers
+            self.circlepattern = np.zeros([height * width, 3])
+            for i in range(0, width):
+                for j in range(0, height):
+                    self.circlepattern[i + j * width, 0] = self.circlepattern_dist * i - \
+                                                           self.circlepattern_dist * (width - 1) / 2
+                    self.circlepattern[i + j * width, 1] = self.circlepattern_dist * j - \
+                                                           self.circlepattern_dist * (height - 1) / 2
+
+    def publish_stop_line_msg(self, header, detected=False, at=False, x=0, y=0):
+        """
+        Makes and publishes a stop line message.
+
+        Args:
+            header: header of a ROS message, usually copied from an incoming message
+            detected (`bool`): whether a vehicle has been detected
+            at (`bool`): whether we are closer than the desired distance
+            x (`float`): distance to the vehicle
+            y (`float`): lateral offset from the vehicle (not used, always 0)
+        """
+
+        stop_line_msg = StopLineReading()
+        stop_line_msg.header = header
+        stop_line_msg.stop_line_detected = detected
+        stop_line_msg.at_stop_line = at
+        stop_line_msg.stop_line_point.x = x
+        stop_line_msg.stop_line_point.y = y
+        self.pub_virtual_stop_line.publish(stop_line_msg)
 
 
 if __name__ == '__main__':
