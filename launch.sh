39fe62d48464c24cec32aa4c41314cd8c010a86e
Andrea F. Daniele <afdaniele@ttic.edu>
2020-05-21 01:04:10 -0500

    updated launch file for atags test

--- launch.sh
+++ launch.sh
@@ -4,4 +4,8 @@ set -e
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
-roslaunch duckietown_demos indefinite_navigation.launch
+#roslaunch duckietown_demos indefinite_navigation.launch
+
+#TODO: revert this
+roslaunch apriltag_ros apriltag_detector_node.launch veh:=watchtower01
+#TODO: revert this
