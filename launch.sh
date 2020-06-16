cb91eb7171ec5d00ace8ba318e3525bacaea3fec
liampaull <paulll@iro.umontreal.ca>
2020-06-16 17:32:28 -0400

    changing default demo

--- launch.sh
+++ launch.sh
@@ -4,4 +4,4 @@ set -e
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
-roslaunch duckietown_demos indefinite_navigation.launch
+roslaunch duckietown_demos lane_following.launch
