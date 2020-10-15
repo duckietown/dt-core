ae5e346c28de36546132f200d3e57fdb1b74987f
frank-qcd-qk <frank_chude.qian@outlook.com>
2020-10-15 17:20:53 -0400

    added legacy wrapper for gpg calls

--- packages/image_processing/include/image_processing/calibration_utils.py
+++ packages/image_processing/include/image_processing/calibration_utils.py
@@ -6,132 +6,9 @@ from sensor_msgs.msg import CameraInfo
 import yaml
 import numpy as np
 import time
-
-class NoHomographyInfoAvailable(dtu.DTException):
-    pass
-
-
-class InvalidHomographyInfo(dtu.DTException):
-    pass
-
-def get_homography_default():
-    """ Returns a nominal homography """
-    return get_homography_for_robot('default')
-
-
-def get_homography_for_robot(robot_name):
-    dtu.check_isinstance(robot_name, str)
-    # find the file
-
-    fn = get_homography_info_config_file(robot_name)
-
-    # load the YAML
-    data = dtu.yaml_load_file(fn,True) # True means "plain"
-
-    # convert the YAML
-    homography = homography_from_yaml(data)
-
-    return homography
-
-def get_homography_info_config_file(robot_name):
-    strict = False
-    roots = [os.path.join(dtu.get_duckiefleet_root(), 'calibrations'),
-             os.path.join(dtu.get_ros_package_path('duckietown'), 'config', 'baseline', 'calibration')]
-
-    found = []
-    for df in roots:
-    # Load camera information
-        fn = os.path.join(df, 'camera_extrinsic', robot_name + '.yaml')
-        fn_default = os.path.join(df, 'camera_extrinsic', 'default.yaml')
-        if os.path.exists(fn):
-            found.append(fn)
-            msg = "Using filename %s" % fn
-            print(msg)
-            dtu.logger.info(msg)
-        elif os.path.exists(fn_default):
-            found.append(fn_default)
-            msg="Using filename %s" % fn_default
-            print(msg)
-            dtu.logger.info(msg)
-
-    if len(found) == 0:
-        msg = 'Cannot find homography file for robot %r;\n%s' % (robot_name, roots)
-        print(msg)
-        raise NoHomographyInfoAvailable(msg)
-    elif len(found) == 1:
-        return found[0]
-    else:
-        msg = 'Found more than one configuration file: \n%s' % "\n".join(found)
-        msg += "\n Please delete one of those."
-        print(msg)
-        if strict:
-            raise Exception(msg)
-        else:
-            dtu.logger.error(msg)
-            return found[0]
-
-def homography_from_yaml(data):
-    try:
-        h = data[b'homography']
-        res = np.array(h).reshape((3, 3))
-        return res
-    except Exception as e:
-        msg = 'Could not interpret data:'
-        msg += '\n\n' + dtu.indent(yaml.dump(data), '   ')
-        print(msg)
-        print(e)
-        dtu.raise_wrapped(InvalidHomographyInfo, e, msg)
-
-def save_homography(H, robot_name):
-    dtu.logger.info('Homography:\n %s' % H)
-
-    # Check if specific point in matrix is larger than zero (this would definitly mean we're having a corrupted rotation matrix)
-    if(H[1][2] > 0):
-        msg = "WARNING: Homography could be corrupt."
-        msg += '\n %s' % H
-        raise Exception(msg)
-
-    ob = {'homography': sum(H.reshape(9, 1).tolist(), [])}
-
-    s = yaml.dump(ob)
-    s += "\n# Calibrated on "
-    localTime = ""+dtu.format_time_as_YYYY_MM_DD(time.time())
-    s += localTime
-
-    fn = get_extrinsics_filename(robot_name)
-
-    dtu.write_data_to_file(s, fn)
-
-
-
-def get_extrinsics_filename(robot_name):
-    fn = dtu.get_duckiefleet_root() + "/calibrations/camera_extrinsic/" + robot_name + ".yaml"
-    return fn
-
-
-def disable_old_homography(robot_name):
-    fn = get_extrinsics_filename(robot_name)
-    if os.path.exists(fn):
-        for i in range(100):
-            fn2 = fn + '.disabled.%03d' % i
-            if not os.path.exists(fn2):
-                break
-        msg = 'Disabling old homography - so that if this fails it is clear it failed.\n Backup saved as %s' % fn2
-        dtu.logger.warning(msg)
-        shutil.move(fn, fn2)
-
-class NoCameraInfoAvailable(dtu.DTException):
-    pass
-
-
-class InvalidCameraInfo(dtu.DTException):
-    pass
-
-
-def get_camera_info_default():
-    """ Returns a nominal CameraInfo """
-    return get_camera_info_for_robot('default')
-
+from .ground_projection_geometry import GroundProjectionGeometry
+from .rectification import Rectify
+import cv2
 
 default_camera_info = """
 image_width: 640
@@ -155,6 +32,136 @@ projection_matrix:
   cols: 4
   data: [220.2460277141687, 0, 301.8668918355899, 0, 0, 238.6758484095299, 227.0880056118307, 0, 0, 0, 1, 0]
 """
+
+
+class NoHomographyInfoAvailable(dtu.DTException):
+    pass
+
+
+class InvalidHomographyInfo(dtu.DTException):
+    pass
+
+
+class NoCameraInfoAvailable(dtu.DTException):
+    pass
+
+
+class InvalidCameraInfo(dtu.DTException):
+    pass
+
+
+def get_homography_default():
+    """ Returns a nominal homography """
+    return get_homography_for_robot('default')
+
+
+def get_homography_for_robot(robot_name):
+    dtu.check_isinstance(robot_name, str)
+    # find the file
+
+    fn = get_homography_info_config_file(robot_name)
+
+    # load the YAML
+    data = dtu.yaml_load_file(fn, True)  # True means "plain"
+
+    # convert the YAML
+    homography = homography_from_yaml(data)
+
+    return homography
+
+
+def get_homography_info_config_file(robot_name):
+    strict = False
+    roots = [os.path.join(dtu.get_duckiefleet_root(), 'calibrations'),
+             os.path.join(dtu.get_ros_package_path('duckietown'), 'config', 'baseline', 'calibration')]
+
+    found = []
+    for df in roots:
+        # Load camera information
+        fn = os.path.join(df, 'camera_extrinsic', robot_name + '.yaml')
+        fn_default = os.path.join(df, 'camera_extrinsic', 'default.yaml')
+        if os.path.exists(fn):
+            found.append(fn)
+            msg = "Using filename %s" % fn
+            print(msg)
+            dtu.logger.info(msg)
+        elif os.path.exists(fn_default):
+            found.append(fn_default)
+            msg = "Using filename %s" % fn_default
+            print(msg)
+            dtu.logger.info(msg)
+
+    if len(found) == 0:
+        msg = 'Cannot find homography file for robot %r;\n%s' % (robot_name, roots)
+        print(msg)
+        raise NoHomographyInfoAvailable(msg)
+    elif len(found) == 1:
+        return found[0]
+    else:
+        msg = 'Found more than one configuration file: \n%s' % "\n".join(found)
+        msg += "\n Please delete one of those."
+        print(msg)
+        if strict:
+            raise Exception(msg)
+        else:
+            dtu.logger.error(msg)
+            return found[0]
+
+
+def homography_from_yaml(data):
+    try:
+        h = data[b'homography']
+        res = np.array(h).reshape((3, 3))
+        return res
+    except Exception as e:
+        msg = 'Could not interpret data:'
+        msg += '\n\n' + dtu.indent(yaml.dump(data), '   ')
+        print(msg)
+        print(e)
+        dtu.raise_wrapped(InvalidHomographyInfo, e, msg)
+
+
+def save_homography(H, robot_name):
+    dtu.logger.info('Homography:\n %s' % H)
+
+    # Check if specific point in matrix is larger than zero (this would definitly mean we're having a corrupted rotation matrix)
+    if (H[1][2] > 0):
+        msg = "WARNING: Homography could be corrupt."
+        msg += '\n %s' % H
+        raise Exception(msg)
+
+    ob = {'homography': sum(H.reshape(9, 1).tolist(), [])}
+
+    s = yaml.dump(ob)
+    s += "\n# Calibrated on "
+    localTime = "" + dtu.format_time_as_YYYY_MM_DD(time.time())
+    s += localTime
+
+    fn = get_extrinsics_filename(robot_name)
+
+    dtu.write_data_to_file(s, fn)
+
+
+def get_extrinsics_filename(robot_name):
+    fn = dtu.get_duckiefleet_root() + "/calibrations/camera_extrinsic/" + robot_name + ".yaml"
+    return fn
+
+
+def disable_old_homography(robot_name):
+    fn = get_extrinsics_filename(robot_name)
+    if os.path.exists(fn):
+        for i in range(100):
+            fn2 = fn + '.disabled.%03d' % i
+            if not os.path.exists(fn2):
+                break
+        msg = 'Disabling old homography - so that if this fails it is clear it failed.\n Backup saved as %s' % fn2
+        dtu.logger.warning(msg)
+        shutil.move(fn, fn2)
+
+
+def get_camera_info_default():
+    """ Returns a nominal CameraInfo """
+    return get_camera_info_for_robot('default')
 
 
 @dtu.contract(robot_name=str, returns=CameraInfo)
@@ -206,10 +213,10 @@ def camera_info_from_yaml(calib_data):
         cam_info = CameraInfo()
         cam_info.width = calib_data[b'image_width']
         cam_info.height = calib_data[b'image_height']
-#         cam_info.K = np.matrix(calib_data['camera_matrix']['data']).reshape((3,3))
-#         cam_info.D = np.matrix(calib_data['distortion_coefficients']['data']).reshape((1,5))
-#         cam_info.R = np.matrix(calib_data['rectification_matrix']['data']).reshape((3,3))
-#         cam_info.P = np.matrix(calib_data['projection_matrix']['data']).reshape((3,4))
+        #         cam_info.K = np.matrix(calib_data['camera_matrix']['data']).reshape((3,3))
+        #         cam_info.D = np.matrix(calib_data['distortion_coefficients']['data']).reshape((1,5))
+        #         cam_info.R = np.matrix(calib_data['rectification_matrix']['data']).reshape((3,3))
+        #         cam_info.P = np.matrix(calib_data['projection_matrix']['data']).reshape((3,4))
         cam_info.K = calib_data[b'camera_matrix'][b'data']
         cam_info.D = calib_data[b'distortion_coefficients'][b'data']
         cam_info.R = calib_data[b'rectification_matrix'][b'data']
@@ -228,7 +235,7 @@ def get_camera_info_config_file(robot_name):
              os.path.join(dtu.get_ros_package_path('duckietown'), 'config', 'baseline', 'calibration')]
 
     for df in roots:
-    # Load camera information
+        # Load camera information
         fn = os.path.join(df, 'camera_intrinsic', robot_name + '.yaml')
         fn_default = os.path.join(df, 'camera_intrinsic', 'default.yaml')
         if os.path.exists(fn):
@@ -277,3 +284,30 @@ def load_camera_info_3(robot):
     cam_info.distortion_model = calib_data['distortion_model']
     dtu.logger.info("Loaded camera calibration parameters for {} from {}".format(robot, os.path.basename(filename)))
     return cam_info
+
+
+class Legacy_GroundProjectionGeometry(object):
+    """
+    This is the wrapper class for old GPG calls.
+    """
+    @dtu.contract(camera_info=CameraInfo, homography='array[3x3]')
+    def __init__(self, camera_info, homography):
+        self.ci = camera_info
+        self.H = homography
+        self.new_gpg = GroundProjectionGeometry(self.ci.width, self.ci.height, self.H)
+        self.new_rectifier = Rectify(self.ci)
+
+    def get_camera_info(self):
+        return self.ci
+
+    def pixel2vector(self, pixel):
+        return self.new_gpg.pixel2vector(pixel)
+
+    def ground2pixel(self, point):
+        return self.new_gpg.ground2pixel(point)
+
+    def rectify(self, cv_image_raw, interpolation=cv2.INTER_NEAREST):
+        return self.new_rectifier.rectify(cv_image_raw, interpolation)
+
+    def distort(self, rectified):
+        return self.new_rectifier.distort(rectified)
