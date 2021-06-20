eee081fb9f6e3c0485c0c4ca554d576d0a789dbd
Andrea Censi <acensi@ethz.ch>
2021-06-20 17:28:09 +0200

    scripts

--- scripts/send-fsm-state.sh
+++ scripts/send-fsm-state.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eux
 STATE=$1
-rostopic pub /${VEHICLE_NAME}/fsm_node/mode duckietown_msgs/FSMState '{header: {}, state: "${STATE}"}'
+rostopic pub "/${VEHICLE_NAME}/fsm_node/mode duckietown_msgs/FSMState" "{header: {}, state: \"${STATE}\"}"
