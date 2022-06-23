7ebd848dbfc124c0bc4fa1ff4c2a7d86df24050e
Andrea F. Daniele <afdaniele@ttic.edu>
2022-06-23 10:59:21 +0100

    minor

--- launchers/event-fos-2022-rht.sh
+++ launchers/event-fos-2022-rht.sh
@@ -13,7 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec roslaunch --wait duckietown_demos ev_fos_lfv_april.launch
+dt-exec roslaunch --wait duckietown_demos event_fos_2022.launch traffic_mode:=RHT
 
 
 # ----------------------------------------------------------------------------
