98ee5c6cfa3587bbc80955f51f52ac0aefac95f0
Jason Hu <jasonhu515@gmail.com>
2022-06-25 10:59:54 +0100

    merge tof into main event launcher

--- launchers/event-fos-2022-rht.sh
+++ launchers/event-fos-2022-rht.sh
@@ -13,7 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec roslaunch --wait duckietown_demos event_fos_2022.launch traffic_mode:=RHT
+dt-exec roslaunch --wait duckietown_demos event_fos_2022_objdet.launch traffic_mode:=RHT
 
 
 # ----------------------------------------------------------------------------
