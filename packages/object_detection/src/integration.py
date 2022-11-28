c67167d021118bc4662c4e15f8d2dabbf73b57c3
Guillaume <g.thibault@polymtl.ca>
2022-11-28 13:26:48 -0500

    fast track ogirin

--- packages/object_detection/src/integration.py
+++ packages/object_detection/src/integration.py
@@ -1,12 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 def DT_TOKEN():
     # todo change this to your duckietown token
-    dt_token = "dt1-3nT8KSoxVh4MdLnE1Bq2mTkhRpbR35G8mmbjExKF7zGm6g4-43dzqWFnWd8KBa1yev1g3UKnzVxZkkTbfYtfGWrfSxeihNZvYVNfNmnCBP28LeqDxL"
+    dt_token = "dt1-3nT8KSoxVh4Migd7N6Nsjy5q8BHtzjcsyz57DKmyii2eBZW-43dzqWFnWd8KBa1yev1g3UKnzVxZkkTbfZNiFAMoUvJ56kg6ydysVET8tiR1aESkdu"
     return dt_token
 
 def MODEL_NAME():
@@ -14,25 +8,17 @@ def MODEL_NAME():
     # if you didn't change it, it should be "yolov5"
     return "yolov5"
 
-# In[2]:
-
-
 def NUMBER_FRAMES_SKIPPED():
     # todo change this number to drop more frames
     # (must be a positive integer)
-    return 0
-
-# In[3]:
-
+    return 1
 
 # `class` is the class of a prediction
 def filter_by_classes(clas):
     # Right now, this returns True for every object's class
     # Change this to only return True for duckies!
     # In other words, returning False means that this prediction is ignored.
-    return True
-
-# In[4]:
+    return False
 
 
 # `scor` is the confidence score of a prediction
@@ -41,8 +27,6 @@ def filter_by_scores(scor):
     # Change this to filter the scores, or not at all
     # (returning True for all of them might be the right thing to do!)
     return True
-
-# In[5]:
 
 
 # `bbox` is the bounding box of a prediction, in xyxy format
