2b2256c4c7f48ecca2600f5f9995684baf4f6614
skarthik <soma.nitt@gmail.com>
2022-12-02 01:20:13 -0500

    changes to check for one transition

--- packages/lane_filter/src/lane_filter_node.py
+++ packages/lane_filter/src/lane_filter_node.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
-import rospy 
+# import json
+
+# import numpy as np
+
+import rospy
+# from cv_bridge import CvBridge
+from duckietown.dtros import DTROS, NodeType, TopicType
+# from duckietown_msgs.msg import FSMState, LanePose, SegmentList, Twist2DStamped
+# from lane_filter import LaneFilterHistogram
+# from sensor_msgs.msg import Image
+# from std_msgs.msg import String
+
+
+class LaneFilterNode(DTROS):
+    
+    def __init__(self, node_name):
+        print("lane_filter_node")
+        super(LaneFilterNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
+        # self.sub_fsm_mode = rospy.Subscriber("~fsm_mode", FSMState, self.cbMode, queue_size=1)
+    
+    # def test_print():
 
 
 if __name__ == "__main__":
-    rospy.init_node("lane_filter_node")
-    rospy.loginfo("lane_filter_node")
+    lane_filter_node = LaneFilterNode(node_name="lane_filter_node")
     rospy.spin()
+    
  
