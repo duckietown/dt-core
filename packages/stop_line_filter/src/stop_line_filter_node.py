bc26cb383652519fa97eb3013a401c2ef655a20f
liampaull <paulll@iro.umontreal.ca>
2023-06-26 15:48:42 -0400

    stop line filter reports pose relative to stop line. Basic testing up to robot stopping at stop line complete. Robustness testing TBD.

--- packages/stop_line_filter/src/stop_line_filter_node.py
+++ packages/stop_line_filter/src/stop_line_filter_node.py
@@ -4,13 +4,41 @@ import numpy as np
 import rospy
 from duckietown.dtros import DTParam, DTROS, NodeType, ParamType
 from duckietown_msgs.msg import BoolStamped, FSMState, LanePose, SegmentList, StopLineReading
-from geometry_msgs.msg import Point
-
-
-# import math
-
+from geometry_msgs.msg import Pose2D
 
 class StopLineFilterNode(DTROS):
+    """
+    Calculates the relative pose of the robot to the stop line and emits an "at_stop_line" boolean if
+    the robot is close enough
+
+    Args:
+        node_name (:obj:`str'): a unique, descriptive name for the node that ROS will use
+
+    Configuration:
+        stop_distance (:obj:`float'): How far from the stop line should we be in order to output the "at_stop_line"
+        output
+        min_segs (:obj:`int'): The minimum number of red line segments that should be detected to constitute a red line
+        detection
+        off_time (:obj:`float'): An amount of time to disable the stop line detection after we leave the stop
+        line and start navigating the intersection. Used so that the robot doesn't immediately redetect that it
+        is at a stop lane before navigating the intersection
+        max_y (:obj:`float'): The maximum offset in the `y` direction (orthogonal to the direction of the lane) to be
+        considered a valid stop line. Used so that we don't stop and stop lines in adjacent lanes
+
+    Subscribers:
+        ~segment_list (:obj:`SegmentList`): The detected line segments from the line detector
+        TODO: subscribe to encoders to update the stop line pose after the stop line is out of the FOV
+        ~lane_pose (:obj:`LanePose'): The Lane Pose output from the lane filter
+        ~fsm_node/mode (:obj:`FSMState'): Our current state
+
+    Publishers:
+        ~stop_line_reading (:obj:`StopLineReading'): Contains booleans for whether a stop line is detected and whether
+        whether we are the stop line (according to the stop distance), and a Pose2D that is our best estimate of the
+        robot pose relative to the stop line. I.e., the coordinate frame being centered on the middle of the
+        stop line with x going forward (down the lane), y going to the left (similar to lane filter), and theta going
+        from x to y (Right-hand rule)
+
+    """
     def __init__(self, node_name):
         # Initialize the DTROS parent class
         super(StopLineFilterNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
@@ -20,12 +48,6 @@ class StopLineFilterNode(DTROS):
         self.min_segs = DTParam("~min_segs", param_type=ParamType.INT)
         self.off_time = DTParam("~off_time", param_type=ParamType.FLOAT)
         self.max_y = DTParam("~max_y", param_type=ParamType.FLOAT)
-
-        ## params
-        # self.stop_distance = self.setupParam("~stop_distance", 0.22) # distance from the stop line that we should stop
-        # self.min_segs      = self.setupParam("~min_segs", 2) # minimum number of red segments that we should detect to estimate a stop
-        # self.off_time      = self.setupParam("~off_time", 2)
-        # self.max_y         = self.setupParam("~max_y ", 0.2) # If y value of detected red line is smaller than max_y we will not set at_stop_line true.
 
         ## state vars
         self.lane_pose = LanePose()
@@ -40,17 +62,6 @@ class StopLineFilterNode(DTROS):
         self.pub_stop_line_reading = rospy.Publisher("~stop_line_reading", StopLineReading, queue_size=1)
         self.pub_at_stop_line = rospy.Publisher("~at_stop_line", BoolStamped, queue_size=1)
 
-    # def setupParam(self,param_name,default_value):
-    #     value = rospy.get_param(param_name,default_value)
-    #     rospy.set_param(param_name,value) #Write to parameter server for transparancy
-    #     rospy.loginfo("[%s] %s = %s " %(self.node_name,param_name,value))
-    #     return value
-    #
-    # def updateParams(self,event):
-    #     self.stop_distance = rospy.get_param("~stop_distance")
-    #     self.min_segs      = rospy.get_param("~min_segs")
-    #     self.off_time      = rospy.get_param("~off_time")
-    #     self.max_y         = rospy.get_param("~max_y")
 
     def cb_state_change(self, msg):
         if (self.state == "INTERSECTION_CONTROL") and (msg.state == "LANE_FOLLOWING"):
@@ -68,13 +79,6 @@ class StopLineFilterNode(DTROS):
         self.sleep = False
         self.loginfo("Resuming stop line detection after the intersection")
 
-    # def cbSwitch(self, switch_msg):
-    #     self.active = switch_msg.data
-    #     if self.active and self.state == "INTERSECTION_CONTROL":
-    #         self.after_intersection_work()
-    #
-    #
-
     def cb_lane_pose(self, lane_pose_msg):
         self.lane_pose = lane_pose_msg
 
@@ -85,7 +89,6 @@ class StopLineFilterNode(DTROS):
 
         good_seg_count = 0
         stop_line_x_accumulator = 0.0
-        stop_line_y_accumulator = 0.0
         for segment in segment_list_msg.segments:
             if segment.color != segment.RED:
                 continue
@@ -96,8 +99,12 @@ class StopLineFilterNode(DTROS):
             p2_lane = self.to_lane_frame(segment.points[1])
             avg_x = 0.5 * (p1_lane[0] + p2_lane[0])
             avg_y = 0.5 * (p1_lane[1] + p2_lane[1])
+
+            # If the line is more than max_y offset in the y direction then it is
+            # not a stop line in our lane and we shouldn't count it
+            if np.abs(avg_y) > self.max_y.value:
+                continue
             stop_line_x_accumulator += avg_x
-            stop_line_y_accumulator += avg_y  # TODO output covariance and not just mean
             good_seg_count += 1.0
 
         stop_line_reading_msg = StopLineReading()
@@ -107,23 +114,17 @@ class StopLineFilterNode(DTROS):
             stop_line_reading_msg.at_stop_line = False
             self.pub_stop_line_reading.publish(stop_line_reading_msg)
 
-            # ### CRITICAL: publish false to at stop line output_topic
-            # msg = BoolStamped()
-            # msg.header.stamp = stop_line_reading_msg.header.stamp
-            # msg.data = False
-            # self.pub_at_stop_line.publish(msg)
-            # ### CRITICAL END
-
         else:
             stop_line_reading_msg.stop_line_detected = True
-            stop_line_point = Point()
-            stop_line_point.x = stop_line_x_accumulator / good_seg_count
-            stop_line_point.y = stop_line_y_accumulator / good_seg_count
-            stop_line_reading_msg.stop_line_point = stop_line_point
+            stop_pose = Pose2D()
+            stop_pose.x = - stop_line_x_accumulator / good_seg_count
+            stop_pose.y = self.lane_pose.d
+            stop_pose.theta = self.lane_pose.phi
+            stop_line_reading_msg.stop_pose = stop_pose
+
             # Only detect redline if y is within max_y distance:
-            stop_line_reading_msg.at_stop_line = (
-                stop_line_point.x < self.stop_distance.value and np.abs(stop_line_point.y) < self.max_y.value
-            )
+            stop_line_reading_msg.at_stop_line = \
+                -stop_pose.x < self.stop_distance.value
 
             self.pub_stop_line_reading.publish(stop_line_reading_msg)
             if stop_line_reading_msg.at_stop_line:
@@ -140,9 +141,6 @@ class StopLineFilterNode(DTROS):
         p_new_homo = T.dot(p_homo)
         p_new = p_new_homo[0:2]
         return p_new
-
-    # def onShutdown(self):
-    #     rospy.loginfo("[StopLineFilterNode] Shutdown.")
 
 
 if __name__ == "__main__":
