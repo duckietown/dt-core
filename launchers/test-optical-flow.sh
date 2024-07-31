fe15415fbf71a70b4ec2224a519e37ddd3d122c5
Davide Iafrate <dvde.iafrate98@gmail.com>
2024-07-31 16:58:41 +0000

    Added integration test of optical_flow node

---
+++ launchers/test-optical-flow.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source /environment.sh
+
+# initialize launch file
+dt-launchfile-init
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+
+
+# NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
+# NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
+
+sudo chown -R duckie /tmp
+sudo chown duckie /data/config/calibrations/camera_extrinsic/
+
+# launching app
+DEBUG=1 dt-exec rostest optical_flow optical_flow.test veh:=testdrone
+
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
+
+# wait for app to end
+dt-launchfile-join
