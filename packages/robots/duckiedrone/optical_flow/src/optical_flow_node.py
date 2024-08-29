6d86834a43768bceac9127968cb3a50b6a56889a
Davide Iafrate <dvde.iafrate98@gmail.com>
2024-08-29 16:03:51 +0000

    Optical flow node now takes as input motion vectors

--- packages/robots/duckiedrone/optical_flow/src/optical_flow_node.py
+++ packages/robots/duckiedrone/optical_flow/src/optical_flow_node.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 
-import os
-from typing import Optional, Union
+from typing import Optional
 
-import cv2
-from dt_computer_vision.camera.homography import HomographyToolkit, Homography
-from dt_computer_vision.camera.types import CameraModel, Pixel, NormalizedImagePoint
+from dt_computer_vision.camera.homography import Homography
+from dt_computer_vision.camera.types import CameraModel
 from dt_computer_vision.ground_projection.ground_projector import GroundProjector
-from dt_computer_vision.ground_projection.types import GroundPoint
 
 import numpy as np
 
@@ -15,155 +12,153 @@ from dt_computer_vision.optical_flow.optical_flow import OpticalFlow
 import rospy
 from cv_bridge import CvBridge
 from duckietown.dtros import DTParam, DTROS, NodeType, ParamType
+from duckietown_msgs.msg import Segment, SegmentList
+from geometry_msgs.msg import Point as PointMsg
+
 from nav_msgs.msg import Odometry
-from sensor_msgs.msg import CompressedImage, Range, CameraInfo
-from dt_computer_vision.ground_projection.rendering import debug_image
+from sensor_msgs.msg import CompressedImage, Range
+from opencv_apps.msg import FlowArrayStamped, Flow
+
 
 class OpticalFlowNode(DTROS):
     def __init__(self, node_name):
         # Initialize the DTROS parent class
-        super(OpticalFlowNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
+        super(OpticalFlowNode, self).__init__(
+            node_name=node_name, node_type=NodeType.PERCEPTION
+        )
 
         # Initialize the parameters
         self.process_frequency = DTParam("~process_frequency", param_type=ParamType.INT)
-        self.track_len = DTParam("~track_len", param_type=ParamType.INT)
-        self.detect_interval = DTParam("~detect_interval", param_type=ParamType.INT)
-        self.base_homography_height = DTParam("~base_homography_height", param_type=ParamType.FLOAT)
-
-        self.resize_scale = DTParam(
-            "~img_scale",
-            default=1.0,
-            help="Scale the input image to the optical flow algorithm by this factor.",
-            param_type=ParamType.FLOAT,
-            max_value=1.0,
-            min_value=0.0
+        self.base_homography_height = DTParam(
+            "~base_homography_height", param_type=ParamType.FLOAT
         )
-        
-        self.scaled_height : float = 1.0
-        self.scaled_width : float = 1.0
+
+        # TODO: make sure this parameter gets loaded at the right time
+        # Retrieve the resize scale from the ROS parameter service
+        self.resize_scale = rospy.get_param("/resized/scale_width")
 
         # obj attrs
         self.bridge = CvBridge()
         self.last_stamp = rospy.Time.now()
-        
-        self.camera : Optional[CameraModel] = None
+
+        self.camera: Optional[CameraModel] = None
         self._camera_info_initialized = False
         self.homography: Optional[Homography] = None
         self.projector: Optional[GroundProjector] = None
 
+        # TODO: this should be deprecated as we are obtaining the optical flow vector from another node
         # optical flow setup
-        self.optical_flow = OpticalFlow(
-            self.track_len.value, self.detect_interval.value, self.resize_scale.value
-        )
-        self._range : float = 0.0
-        self._scale : float = self._range / self.base_homography_height.value
+        self.optical_flow = OpticalFlow(1, 1, self.resize_scale)
+        self.motion_vectors: Optional[np.ndarray] = None
+        self.locations_px: Optional[np.ndarray] = None
+
+        self._range: float = 0.0
+        self._scale: float = self._range / self.base_homography_height.value
 
         # Publishers
-        self.pub_debug_image = rospy.Publisher("~debug/image/compressed", CompressedImage, queue_size=1)
-        self.pub_debug_projected_image = rospy.Publisher("~debug/image/projected/compressed", CompressedImage, queue_size=1)
+        self.pub_debug_image = rospy.Publisher(
+            "~debug/image/compressed", CompressedImage, queue_size=1
+        )
         self.pub_odometry = rospy.Publisher("~visual_odometry", Odometry, queue_size=1)
+        self.pub_motion_vectors = rospy.Publisher(
+            "~lineseglist_out", SegmentList, queue_size=1
+        )
 
         # Subscriber
-        self.sub_image = rospy.Subscriber("~image", CompressedImage, self.cb_image, queue_size=1)
-        self.sub_camera_info = rospy.Subscriber("~camera_info", CameraInfo, self.cb_camera_info, queue_size=1)
-        self.sub_height = rospy.Subscriber("~range", Range, self.cb_new_range, queue_size=1)
+        self.sub_motion_vectors = rospy.Subscriber(
+            "~motion_vectors", FlowArrayStamped, self.cb_motion_vectors, queue_size=1
+        )
+        self.sub_range = rospy.Subscriber(
+            "~range", Range, self.cb_new_range, queue_size=1
+        )
+        self.sub_projected_motion_vectors = rospy.Subscriber(
+            "~projected_motion_vectors",
+            SegmentList,
+            self.cb_projected_motion_vectors,
+            queue_size=1,
+        )
 
         self.loginfo("Initialization completed.")
 
-    def cb_camera_info(self, msg: CameraInfo):
-        if not self._camera_info_initialized:
-            self.camera = CameraModel(
-                height=msg.height,
-                width=msg.width,
-                K=np.array(msg.K).reshape((3, 3)),
-                D=np.array(msg.D),
-                R=np.array(msg.R).reshape((3, 3)),
-                P=np.array(msg.P).reshape((3, 4)),
-            )
-            
-            # TODO: remove this hardcoded homography, fix the homography loading
-            self.camera.H = Homography([[7.68649577e-02, 4.80407934e-02, 4.79735399e-02],
-                                [2.35921238e-04, 1.98051018e-01, 1.04001771e-01],
-                                [3.19667170e-02, 9.15595543e-01, 1.00000000e+00]])
+    def cb_motion_vectors(self, msg: FlowArrayStamped):
+        """
+        Callback for the motion vectors from the optical flow algorithm.
+        Repack them as SegmentList and publish them to the ground projector.
+        """
+        segment_list = SegmentList()
+        segment_list.header = msg.header
 
-            self.loginfo(f"Camera model initialized: {self.camera}")
-            self.projector = GroundProjector(self.camera)
-
-
-        self._camera_info_initialized = True
-
-    def cb_image(self, image_msg : CompressedImage):
-        if not self._camera_info_initialized:
-            rospy.logdebug("Cannot process image, camera info not initialized.")
+        if msg.flow is None:
+            rospy.logwarn("No motion vectors received.")
             return
+
+        segment_list.segments = []
+
+        for flow in msg.flow:
+            flow: Flow
+            segment = Segment(
+                points=[
+                    PointMsg(x=flow.point.x, y=flow.point.y),
+                    PointMsg(
+                        x=flow.point.x + flow.velocity.x * 1,
+                        y=flow.point.y + flow.velocity.y * 1,
+                    ),
+                ]
+            )
+            segment_list.segments.append(segment)
+
+        self.pub_motion_vectors.publish(segment_list)
+
+    def cb_projected_motion_vectors(self, msg: SegmentList):
+        """
+        Callback for the motion vectors from the optical flow algorithm.
+        It grabs the projected motion vectors and computes the velocity vector using a mean.
+
+        Args:
+            msg: the SegmentList message containing the projected motion vectors
+
+        """
 
         now = rospy.Time.now()
-        if now - self.last_stamp < rospy.Duration.from_sec(1.0 / self.process_frequency.value):
+        # if now - self.last_stamp < rospy.Duration.from_sec(1.0 / self.process_frequency.value):
+        #     return
+
+        # TODO: We need to scale these depending on the parameter set in the image resize node
+
+        if msg.segments is None:
+            rospy.logwarn_throttle(period=1, msg="Empty motion vectors array received.")
             return
 
-        t_now = now.to_sec()
-        t_stamp = image_msg.header.stamp.to_sec()
-        if abs(t_now - t_stamp) > 0.3:
-            return
+        if self.motion_vectors is None:
+            num_motion_vectors = len(msg.segments)
+            self.motion_vectors = np.zeros((2, num_motion_vectors))
+            self.locations_px = np.zeros((2, num_motion_vectors))
 
-        image = self.bridge.compressed_imgmsg_to_cv2(image_msg, "bgr8")
-        delta_t = float(t_now - self.last_stamp.to_sec())
+        for i, flow in enumerate(msg.segments):
+            flow: Segment
+            self.motion_vectors[:, i] = np.array(
+                [
+                    flow.points[1].x - flow.points[0].x,
+                    flow.points[1].y - flow.points[0].y,
+                ]
+            )
 
-        image = self.camera.rectifier.rectify(image)
-        
-        # Compute the optical flow
-        displacements_array, motion_vectors, locations_px, debug_str = (
-            self.optical_flow.compute_motion_vectors(image, delta_t)
-        )
-        
         if self.pub_debug_image.get_num_connections() > 0:
+            # TODO: implement visualization
+            return
             vis = self.optical_flow.create_debug_visualization(
-                image, locations_px, debug_str, motion_vectors
+                msg, self.locations_px, debug_str, self.motion_vectors
             )
             self.pub_debug_image.publish(self.bridge.cv2_to_compressed_imgmsg(vis))
 
-####################### TODO: Separate projection from Optical Flow node to Ground Projection node
+        velocity = np.mean(np.array(self.motion_vectors), axis=0)
 
-        colored_segments = {(255, 255, 255): []}
-
-        for loc, mo_vec in zip(locations_px, motion_vectors):
-            # distorted pixels
-            p0: Pixel = loc * self.resize_scale.value
-            p1: Pixel = (loc + mo_vec) * self.resize_scale.value
-            
-            # rectified pixel to normalized coordinates
-            p0_norm: NormalizedImagePoint = self.camera.pixel2vector(p0)
-            p1_norm: NormalizedImagePoint = self.camera.pixel2vector(p1)
-
-            #print(p0_norm)
-            # project image point onto the ground plane
-            p0_ground: GroundPoint = self.projector.vector2ground(p0_norm)
-            p1_ground: GroundPoint = self.projector.vector2ground(p1_norm)
-            
-            
-            # add grounded segment to output
-            colored_segments[(255, 255, 255)].append((p0_ground, p1_ground))
-
-        if self.pub_debug_projected_image.get_num_connections() > 0:
-            image_w_projected_segments = debug_image(colored_segments, (300, 300), grid_size=2, s_segment_thickness=2, resolution=0.1,start_y = 0.2)
-            image_w_projected_segments = cv2.cvtColor(image_w_projected_segments, cv2.COLOR_BGR2RGB)
-            self.pub_debug_projected_image.publish(
-                self.bridge.cv2_to_compressed_imgmsg(image_w_projected_segments)
-                )
-#######################
-
-        projected_motion_vectors = [p1-p0 for p0, p1 in colored_segments[(255, 255, 255)]]
-
-        if debug_str:
-            rospy.logdebug(debug_str)
-
-        velocity = self.optical_flow.compute_velocity_vector(projected_motion_vectors) * self._scale
+        # velocity = self.optical_flow.compute_velocity_vector(self.motion_vectors)
 
         # Rotate the velocity vector 90 degrees clockwise to match the odometry frame
         velocity = np.array([velocity[1], velocity[0]])
 
-        # Remove one dimension in the array
-        assert velocity.shape == (2,) , f"Velocity: {velocity}"
+        assert velocity.shape == (2,), f"Velocity: {velocity}"
         self.logdebug(f"Computed velocity vector [m/s]: {velocity}")
 
         # Publish the optical flow vector as odometry
@@ -173,49 +168,15 @@ class OpticalFlowNode(DTROS):
         # TODO: change this to the correct frame
         odometry_msg.child_frame_id = "base_link"
         odometry_msg.twist.twist.linear.x = velocity[0]
-        odometry_msg.twist.twist.linear.y = velocity[1] 
+        odometry_msg.twist.twist.linear.y = velocity[1]
 
         self.pub_odometry.publish(odometry_msg)
 
-        self.last_stamp = now
+        # self.last_stamp = now
 
-    def cb_new_range(self, msg : Range):
+    def cb_new_range(self, msg: Range):
         self._range = msg.range
         self._scale = self._range / self.base_homography_height.value
-        
-    ##########################
-    def load_homography(self) -> Union[Homography, None]:
-        """
-        Loads the homography matrix from the extrinsic calibration file.
-
-        Returns:
-            :obj:`Homography`: the loaded homography matrix
-
-        """
-        # load extrinsic calibration
-        cali_file_folder = "/data/config/calibrations/camera_extrinsic/"
-        cali_file = cali_file_folder + rospy.get_namespace().strip("/") + ".yaml"
-
-        # Locate calibration yaml file or use the default otherwise
-        if not os.path.isfile(cali_file):
-            self.log(
-                f"Can't find calibration file: {cali_file}\n Using default calibration instead.", "warn"
-            )
-            cali_file = os.path.join(cali_file_folder, "default.yaml")
-
-        # Shutdown if no calibration file not found
-        if not os.path.isfile(cali_file):
-            msg = "Found no calibration file ... aborting"
-            self.logerr(msg)
-            rospy.signal_shutdown(msg)
-
-        try:
-            H : Homography = HomographyToolkit.load_from_disk(cali_file, return_date=False) # type: ignore
-            return H.reshape((3, 3))
-        except Exception as e:
-            msg = f"Error in parsing calibration file {cali_file}:\n{e}"
-            self.logerr(msg)
-            rospy.signal_shutdown(msg)
 
 
 if __name__ == "__main__":
