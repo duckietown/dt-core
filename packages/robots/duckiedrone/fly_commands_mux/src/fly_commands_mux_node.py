91b5fc4684e6b0ea1af10df8ed82039a19caa492
Davide Iafrate <dvde.iafrate98@gmail.com>
2024-09-11 08:50:53 +0000

    Refactor fly_commands_mux_node.py to use OverrideRCIn.CHAN_NOCHANGE for unused channels

--- packages/robots/duckiedrone/fly_commands_mux/src/fly_commands_mux_node.py
+++ packages/robots/duckiedrone/fly_commands_mux/src/fly_commands_mux_node.py
@@ -74,6 +74,7 @@ class FlyCommandsMuxNode(DTROS):
                 return
             else:  # only auto commands
                 msg = OverrideRCIn()
+                msg.channels = [OverrideRCIn.CHAN_NOCHANGE] * len(OverrideRCIn.channels)
                 msg.channels[0] = int(self.autonomous_commands.roll)
                 msg.channels[1] = int(self.autonomous_commands.pitch)
                 msg.channels[3] = int(self.autonomous_commands.yaw)
@@ -82,6 +83,7 @@ class FlyCommandsMuxNode(DTROS):
         else:
             if self.autonomous_commands is None:
                 msg = OverrideRCIn()
+                msg.channels = [OverrideRCIn.CHAN_NOCHANGE] * len(OverrideRCIn.channels)
                 msg.channels[0] = int(self.manual_commands.roll)
                 msg.channels[1] = int(self.manual_commands.pitch)
                 msg.channels[3] = int(self.manual_commands.yaw)
@@ -89,6 +91,7 @@ class FlyCommandsMuxNode(DTROS):
                 self.pub_cmds.publish(msg)
             else:  # when both are valid
                 msg = OverrideRCIn()
+                msg.channels = [OverrideRCIn.CHAN_NOCHANGE] * len(OverrideRCIn.channels)
                 self.logdebug((
                     "Masks (r p y t): "
                     f"{self.r_override.value} "
