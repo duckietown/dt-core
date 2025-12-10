69bddd9ed329f65a901afae8d9769b37b8bf1dc7
Mohammad Houdeib <mhoudeib@bflcanada.ca>
2025-12-09 21:24:23 -0500

    fix intersection paralysis by adding pivot recovery when no AprilTag is detected

--- packages/navigation/src/intersection_type_detector_node.py
+++ packages/navigation/src/intersection_type_detector_node.py
@@ -6,7 +6,7 @@ import math
 import numpy
 
 import rospy
-from duckietown_msgs.msg import AprilTagsWithInfos, FSMState, TurnIDandType, BoolStamped
+from duckietown_msgs.msg import AprilTagsWithInfos, FSMState, TurnIDandType, BoolStamped, WheelsCmdStamped
 from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
 
 
@@ -23,6 +23,15 @@ class IntersectionTypeDetectorNode(DTROS):
         self.turn_type = -1
         rospy.loginfo(f"[{self.node_name}] Initializing.")
 
+        # Parameters for pivot scan behavior ---
+        # speed: wheel speed magnitude for pivot (left = +s, right = -s)
+        self.scan_speed = 0.05
+        # duration: how long to rotate (in seconds) when no tag is detected
+        self.scan_duration = 0.5
+        # Internal scan state
+        self._scan_active = False
+        self._scan_end_time = rospy.Time(0)
+
         # Setup publishers
         self.pub_stop_sign = rospy.Publisher(
             "~stop_sign_intersection_detected",
@@ -37,12 +46,39 @@ class IntersectionTypeDetectorNode(DTROS):
             queue_size=1,
             latch=True
         )
+        
+        self.pub_wheels = rospy.Publisher(
+            "wheels_driver_node/wheels_cmd",
+            WheelsCmdStamped,
+            queue_size=1
+        )
 
         # Setup subscribers
         self.sub_topic_tag = rospy.Subscriber("~tag", AprilTagsWithInfos, self.cbTag, queue_size=1)
 
         rospy.loginfo(f"[{self.node_name}] Initialzed.")
 
+    def _publish_wheels(self, v_left, v_right):
+        """Publish a WheelsCmdStamped to the wheels driver."""
+        msg = WheelsCmdStamped()
+        msg.header.stamp = rospy.Time.now()
+        msg.vel_left = v_left
+        msg.vel_right = v_right
+        self.pub_wheels.publish(msg)
+
+    def _start_scan(self):
+        """Begin a pivot scan: rotate in place for scan_duration seconds."""
+        self._scan_active = True
+        self._scan_end_time = rospy.Time.now() + rospy.Duration(self.scan_duration)
+        rospy.loginfo(f"[{self.node_name}] Starting pivot scan to search for intersection sign.")
+
+    def _stop_scan(self):
+        """Stop any ongoing scan and send zero-velocity command."""
+        if self._scan_active:
+            rospy.loginfo(f"[{self.node_name}] Stopping pivot scan.")
+        self._scan_active = False
+        # send zero wheel command to stop rotation
+        self._publish_wheels(0.0, 0.0)
 
     def cbTag(self, tag_msgs):
             # loop through list of april tags to
@@ -62,8 +98,23 @@ class IntersectionTypeDetectorNode(DTROS):
 
             if idx_min == -1:
                 rospy.logwarn("[INTERSECTION_TYPE_DETECTOR_NODE]: Unable to determine intersection type, "
-                              "no traffic light or stop sign signs detected")
+                              "no traffic light or stop sign signs detected. Duckiebot will pivot until one is detected")
+                now = rospy.Time.now()
+
+                # If we are not already scanning, start a scan
+                if not self._scan_active:
+                    self._start_scan()
+
+                # While scanning and within the scan window, keep publishing pivot command
+                if self._scan_active and now < self._scan_end_time:
+                    # Pivot in place: left wheel forward, right wheel backward
+                    s = self.scan_speed
+                    self._publish_wheels(s, -s)
+                else:
+                    # Scan window over: stop the scan and stop wheels
+                    self._stop_scan()
             else:
+                self._stop_scan()
                 header = tag_msgs.header
                 to_pub = BoolStamped()
                 to_pub.header=header
